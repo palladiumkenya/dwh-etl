dc0435b8afca1b802d304a6fefcc9e4ead7512a9
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2022-12-08 11:17:34 +0300

    Script cleanup Fix broken references

--- Scripts/ODS/load_intermediate_tables/load_intermediate_PregnancyDuringART.sql
+++ Scripts/ODS/load_intermediate_tables/load_intermediate_PregnancyDuringART.sql
@@ -5,32 +5,31 @@ SELECT
     SiteCode,
     MAX(PregnantDuringART)AS PregnantDuringART,
      X.VisitDate,
-     StartARTDate
+     StartARTDate,
+	 cast(getdate() as date) as LoadDate
 FROM 
 (
 SELECT DISTINCT 
     Patients.PatientID ,
     Patients.PatientPK ,
     Patients.SiteCode, 
-    VisitDategit ad,
+    VisitDate,
     StartARTDate,
-		CASE WHEN VisitDate >= ART.StartARTDate THEN 1 ELSE 0 END AS PregnantDuringART,
-         cast(getdate() as date) as LoadDate
- FROM stg_PatientVisits Visits
- INNER JOIN stg_Patients Patients ON Visits.PatientID=Patients.PatientID AND Visits.PatientPK=Patients.PatientPK AND Patients.SiteCode=Visits.SiteCode
- INNER JOIN stg_ARTPatients ART ON ART.PatientID=Patients.PatientID AND ART.PatientPK=Patients.PatientPK AND Patients.SiteCode=ART.SiteCode
+		CASE WHEN VisitDate >= ART.StartARTDate THEN 1 ELSE 0 END AS PregnantDuringART
+ FROM CT_PatientVisits Visits
+ INNER JOIN CT_Patient Patients ON Visits.PatientID=Patients.PatientID AND Visits.PatientPK=Patients.PatientPK AND Patients.SiteCode=Visits.SiteCode
+ INNER JOIN CT_ARTPatients ART ON ART.PatientID=Patients.PatientID AND ART.PatientPK=Patients.PatientPK AND Patients.SiteCode=ART.SiteCode
   WHERE Visits.Pregnant = 'Yes' OR Visits.Pregnant = 'Y'
   
 ) X 
-GROUP BY PatientID ,PatientPK ,SiteCode,VisitDate,LMP,EDD,SiteCode,StartARTDate
+GROUP BY PatientID ,PatientPK ,SiteCode,VisitDate,SiteCode,StartARTDate
 )
-
 Select 
         PregnancyDuringART.PatientID ,
         PregnancyDuringART.PatientPK ,
         PregnancyDuringART.SiteCode,
-        PregnancyDuringART.PregnancyDuringART,
+        PregnancyDuringART.PregnantDuringART,
         PregnancyDuringART.LoadDate
- INTO dbo.PregnancyDuringART
-FROM   PregnancyAsAT
+ INTO dbo.Intermediate_PregnancyDuringART
+FROM  PregnancyDuringART
 
