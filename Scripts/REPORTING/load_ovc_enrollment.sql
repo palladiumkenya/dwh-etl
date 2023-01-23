5b9173e3f4506ae3e287020869caa15a8bb9d741
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2023-01-23 13:55:45 +0300

    Add ovc count report

--- Scripts/REPORTING/load_ovc_enrollment.sql
+++ Scripts/REPORTING/load_ovc_enrollment.sql
@@ -1,4 +1,4 @@
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[REPORTING].[dbo].[FACT_Trans_OVCEnrollments]') AND type in (N'U'))
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[REPORTING].[dbo].[OVCEnrollments]') AND type in (N'U'))
 TRUNCATE TABLE [REPORTING].[dbo].OVCEnrollments
 GO
 
@@ -39,7 +39,6 @@ end as LastRegimen,
  EligibleVL,
 Last12MonthVL as VLDone,
 Last12MVLSup as VirallySuppressed
---INTO [REPORTING].dbo.OVCEnrollment
 from [NDWH].[dbo].[FactOVC] it
 INNER JOIN NDWH.dbo.DimDate enrld on enrld.DateKey = it.OVCEnrollmentDateKey
 INNER join NDWH.dbo.DimFacility f on f.FacilityKey = it.FacilityKey
@@ -48,11 +47,11 @@ INNER JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = it.PatientKey
 INNER JOIN NDWH.dbo.DimPartner p on p.PartnerKey = it.PartnerKey
 INNER JOIN NDWH.dbo.FactART art on art.PatientKey = it.PatientKey
 INNER JOIN NDWH.dbo.FactViralLoads vl on vl.PatientKey = it.PatientKey
-INNER join NDWH.dbo.DimAgeGroup g on g.Age= DATEDIFF(yy,pat.DOB, art.LastVisitDate)
+INNER join NDWH.dbo.DimAgeGroup g on g.Age = AgeLastVisit
 LEFT JOIN NDWH.dbo.DimDate exd on exd.DateKey = it.OVCExitDateKey
 LEFT JOIN NDWH.dbo.DimDate lvd on lvd.DateKey = vl.LastVLDateKey
 LEFT JOIN NDWH.dbo.DimDate fvd on fvd.DateKey = vl.FirstVLDateKey
 LEFT JOIN NDWH.dbo.DimDate lv12md on lv12md.DateKey = vl.Last12MVLDateKey
 LEFT JOIN NDWH.dbo.DimRelationshipWithPatient rp on rp.RelationshipWithPatientKey = it.RelationshipWithPatientKey
 LEFT JOIN NDWH.dbo.FactLatestObs lo on lo.PatientKey = it.PatientKey
-where  DATEDIFF(yy,pat.DOB, art.LastVisitDate) between 0 and 17 and OVCExitReason is null
+where AgeLastVisit between 0 and 17 and OVCExitReason is null
