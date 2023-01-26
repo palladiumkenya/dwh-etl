d2649cd39d282b25c446bb17be4e5379b3f7de73
Paul Nthusi <paul.nthusi@thepalladiumgroup.com>
2023-01-26 10:11:14 +0300

    Update art outcome reference in ovc count report

--- Scripts/REPORTING/load_aggregate_ovc_count.sql
+++ Scripts/REPORTING/load_aggregate_ovc_count.sql
@@ -13,8 +13,8 @@ a.AgencyName as CTAgency,
 Gender, 
 g.DATIMAgeGroup,
 pat.IsTXCurr as TXCurr,
- ARTOutcome,
- count(*) as OVCElligiblePatientCount
+ao.ARTOutcome,
+count(*) as OVCElligiblePatientCount
 from [NDWH].[dbo].[FactOVC] it
 INNER JOIN NDWH.dbo.DimDate enrld on enrld.DateKey = it.OVCEnrollmentDateKey
 INNER join NDWH.dbo.DimFacility f on f.FacilityKey = it.FacilityKey
@@ -22,6 +22,7 @@ INNER JOIN NDWH.dbo.DimAgency a on a.AgencyKey = it.AgencyKey
 INNER JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = it.PatientKey
 INNER JOIN NDWH.dbo.DimPartner p on p.PartnerKey = it.PartnerKey
 INNER JOIN NDWH.dbo.FactART art on art.PatientKey = it.PatientKey
+INNER JOIN NDWH.dbo.DimARTOutcome ao on ao.ARTOutcomeKey = art.ARTOutcomeKey
 LEFT join NDWH.dbo.DimAgeGroup g on g.Age = art.AgeLastVisit
 where art.AgeLastVisit between 0 and 17 and OVCExitReason is null and pat.IsTXCurr = 1
-GROUP BY MFLCode,f.FacilityName,County,Subcounty,p.PartnerName,a.AgencyName,Gender,g.DATIMAgeGroup,pat.IsTXCurr, ARTOutcome
+GROUP BY MFLCode,f.FacilityName,County,Subcounty,p.PartnerName,a.AgencyName,Gender,g.DATIMAgeGroup,pat.IsTXCurr, ao.ARTOutcome
