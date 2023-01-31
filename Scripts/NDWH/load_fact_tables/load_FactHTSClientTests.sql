d349380b7df3404139c4ce114abb427707afa492
Nobert Mumo <mumongungu@gmail.com>
2023-01-31 22:05:40 +0300

    adding a date filter

--- Scripts/NDWH/load_fact_tables/load_FactHTSClientTests.sql
+++ Scripts/NDWH/load_fact_tables/load_FactHTSClientTests.sql
@@ -25,52 +25,52 @@ select
 	partner.PartnerKey,
 	agency.AgencyKey,
     age_group.AgeGroupKey,
-    datediff(yy, patient.DOB, last_encounter.TestDate) as AgeAtTesting,
+    datediff(yy, patient.DOB, hts_encounter.TestDate) as AgeAtTesting,
     testing.DateKey as DateTestedKey,
-    EverTestedForHiv,
-    MonthsSinceLastTest,
-    ClientTestedAs,
-    EntryPoint,
-    TestStrategy,
-    TestResult1,
-    TestResult2,
-    FinalTestResult,
-    PatientGivenResult,
-    TestType,
-    TBScreening,
-    ClientSelfTested,
-    CoupleDiscordant
-    Consent,
-    EncounterId,
-    case when FinalTestResult is not null then 1 else 0 end as Tested,
-    case when FinalTestResult = 'Positive' then 1 else 0 end as Positive,
-    case when (FinalTestResult = 'Positive' and client_linkage_data.ReportedCCCNumber is not null) then 1 else 0 end as Linked,
+    hts_encounter.EverTestedForHiv,
+    hts_encounter.MonthsSinceLastTest,
+    hts_encounter.ClientTestedAs,
+    hts_encounter.EntryPoint,
+    hts_encounter.TestStrategy,
+    hts_encounter.TestResult1,
+    hts_encounter.TestResult2,
+    hts_encounter.FinalTestResult,
+    hts_encounter.PatientGivenResult,
+    hts_encounter.TestType,
+    hts_encounter.TBScreening,
+    hts_encounter.ClientSelfTested,
+    hts_encounter.CoupleDiscordant,
+    hts_encounter.Consent,
+    hts_encounter.EncounterId,
+    case when hts_encounter.FinalTestResult is not null then 1 else 0 end as Tested,
+    case when hts_encounter.FinalTestResult = 'Positive' then 1 else 0 end as Positive,
+    case when (hts_encounter.FinalTestResult = 'Positive' and client_linkage_data.ReportedCCCNumber is not null) then 1 else 0 end as Linked,
     case 
-        when (MonthsSinceLastTest < 3 and MonthsSinceLastTest is not null) then '<3 Months' 
-        when (MonthsSinceLastTest >= 3 and MonthsSinceLastTest < 6) then '3-6 Months' 
-        when (MonthsSinceLastTest >= 6 and MonthsSinceLastTest < 9) then '6-9 Months' 
-        when (MonthsSinceLastTest >= 9 and MonthsSinceLastTest < 12) then '9-12 Months' 
-        when (MonthsSinceLastTest >= 12 and MonthsSinceLastTest < 18) then '12-18 Months' 
-        when (MonthsSinceLastTest >= 18 and MonthsSinceLastTest < 24) then '18-24 Months' 
-        when (MonthsSinceLastTest >= 24 and MonthsSinceLastTest < 36) then '24-36 Months' 
-        when (MonthsSinceLastTest >= 36 and (MonthsSinceLastTest < 48)) then '36-48 Months' 
-        when (MonthsSinceLastTest >= 48 and MonthsSinceLastTest is not null) then '>48Months' 
+        when (hts_encounter.MonthsSinceLastTest < 3 and hts_encounter.MonthsSinceLastTest is not null) then '<3 Months' 
+        when (hts_encounter.MonthsSinceLastTest >= 3 and hts_encounter.MonthsSinceLastTest < 6) then '3-6 Months' 
+        when (hts_encounter.MonthsSinceLastTest >= 6 and hts_encounter.MonthsSinceLastTest < 9) then '6-9 Months' 
+        when (hts_encounter.MonthsSinceLastTest >= 9 and hts_encounter.MonthsSinceLastTest < 12) then '9-12 Months' 
+        when (hts_encounter.MonthsSinceLastTest >= 12 and hts_encounter.MonthsSinceLastTest < 18) then '12-18 Months' 
+        when (hts_encounter.MonthsSinceLastTest >= 18 and hts_encounter.MonthsSinceLastTest < 24) then '18-24 Months' 
+        when (hts_encounter.MonthsSinceLastTest >= 24 and hts_encounter.MonthsSinceLastTest < 36) then '24-36 Months' 
+        when (hts_encounter.MonthsSinceLastTest >= 36 and (hts_encounter.MonthsSinceLastTest < 48)) then '36-48 Months' 
+        when (hts_encounter.MonthsSinceLastTest >= 48 and hts_encounter.MonthsSinceLastTest is not null) then '>48Months' 
     end as MonthsLastTest,
     case 
-        when (EverTestedForHiv = 'Yes' and MonthsSinceLastTest < 12) then 'Retest' 
+        when (hts_encounter.EverTestedForHiv = 'Yes' and hts_encounter.MonthsSinceLastTest < 12) then 'Retest' 
     else 'New' end as TestedBefore
 into NDWH.dbo.FactHTSClientTests
-from ODS.dbo.Intermediate_EncounterHTSTests as last_encounter
-left join NDWH.dbo.DimPatient as patient on patient.PatientPK = convert(nvarchar(64), hashbytes('SHA2_256', cast(last_encounter.PatientPK as nvarchar(36))), 2)
-    and patient.SiteCode = last_encounter.SiteCode
-left join NDWH.dbo.DimFacility as facility on facility.MFLCode = last_encounter.SiteCode
-left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = last_encounter.SiteCode
+from ODS.dbo.Intermediate_EncounterHTSTests as hts_encounter
+left join NDWH.dbo.DimPatient as patient on patient.PatientPK = convert(nvarchar(64), hashbytes('SHA2_256', cast(hts_encounter.PatientPK as nvarchar(36))), 2)
+    and patient.SiteCode = hts_encounter.SiteCode
+left join NDWH.dbo.DimFacility as facility on facility.MFLCode = hts_encounter.SiteCode
+left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = hts_encounter.SiteCode
 left join NDWH.dbo.DimPartner as partner on partner.PartnerName = MFL_partner_agency_combination.SDP
 left join NDWH.dbo.DimAgency as agency on agency.AgencyName = MFL_partner_agency_combination.Agency
-left join NDWH.dbo.DimAgeGroup as age_group on age_group.Age =  datediff(yy, patient.DOB, last_encounter.TestDate)
-left join NDWH.dbo.DimDate as testing on testing.Date = cast(last_encounter.TestDate as date)
-left join  client_linkage_data on client_linkage_data.PatientPk = last_encounter.PatientPK
-    and client_linkage_data.SiteCode = last_encounter.SiteCode
+left join NDWH.dbo.DimAgeGroup as age_group on age_group.Age =  datediff(yy, patient.DOB, hts_encounter.TestDate)
+left join NDWH.dbo.DimDate as testing on testing.Date = cast(hts_encounter.TestDate as date)
+left join  client_linkage_data on client_linkage_data.PatientPk = hts_encounter.PatientPK
+    and client_linkage_data.SiteCode = hts_encounter.SiteCode
     and client_linkage_data.row_num = 1
 
 END
