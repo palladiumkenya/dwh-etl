1788ba08f1793df63b814b151cd0585d25308270
cbrianbet <cbrianbet@outlook.com>
2023-01-31 15:58:20 +0300

    Update load_aggregate_otz.sql

--- Scripts/REPORTING/load_aggregate_otz.sql
+++ Scripts/REPORTING/load_aggregate_otz.sql
@@ -52,4 +52,4 @@ INNER JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = otz.PatientKey
 INNER JOIN NDWH.dbo.DimPartner p on p.PartnerKey = otz.PartnerKey
 LEFT JOIN NDWH.dbo.FactViralLoads vl on vl.PatientKey = otz.PatientKey and vl.PatientKey IS NOT NULL
 WHERE age.Age BETWEEN 10 AND 24 AND IsTXCurr = 1
-GROUP BY MFLCode, f.FacilityName, County, SubCounty, p.PartnerName, a.AgencyName, Gender, age.DATIMAgeGroup, CONVERT(char(7), cast(cast(OTZEnrollmentDateKey as char) as datetime), 23), TransferInStatus, ModulesPreviouslyCovered, vl.FirstVL, vl.LastVL, vl.Last12MonthVLResults, Last12MVLResult
+GROUP BY MFLCode, f.FacilityName, County, SubCounty, p.PartnerName, a.AgencyName, Gender, age.DATIMAgeGroup, CONVERT(char(7), cast(cast(OTZEnrollmentDateKey as char) as datetime), 23), TransferInStatus, ModulesPreviouslyCovered, vl.FirstVL, vl.LastVL, vl.Last12MonthVLResults
