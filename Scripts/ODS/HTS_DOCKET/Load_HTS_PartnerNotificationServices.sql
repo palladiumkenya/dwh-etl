13e828bd864b0ee13a78bfff593ea481fa197517
cbrianbet <cbrianbet@outlook.com>
2023-01-31 09:22:36 +0330

    Updates

--- Scripts/ODS/HTS_DOCKET/Load_HTS_PartnerNotificationServices.sql
+++ Scripts/ODS/HTS_DOCKET/Load_HTS_PartnerNotificationServices.sql
@@ -1,4 +1,5 @@
 BEGIN
+		--truncate table [ODS].[dbo].[HTS_PartnerNotificationServices]
 		MERGE [ODS].[dbo].[HTS_PartnerNotificationServices] AS a
 			USING(SELECT DISTINCT a.[FacilityName]
 				  ,a.[SiteCode]
@@ -30,7 +31,24 @@ BEGIN
 			  ) AS b 
 			ON(
 				a.PatientPK  = b.PatientPK 
-			and a.SiteCode = b.SiteCode						
+				and a.SiteCode = b.SiteCode	
+				
+				and a.PartnerPersonID  = b.PartnerPersonID 
+				and a.PartnerPatientPk  = b.PartnerPatientPk 
+
+				and a.HtsNumber COLLATE Latin1_General_CI_AS = b.HtsNumber 
+				and a.KnowledgeOfHivStatus COLLATE Latin1_General_CI_AS = b.KnowledgeOfHivStatus  
+				and a.PnsApproach COLLATE Latin1_General_CI_AS = b.PnsApproach 
+				and a.MaritalStatus COLLATE Latin1_General_CI_AS = b.MaritalStatus 
+				and a.RelationsipToIndexClient COLLATE Latin1_General_CI_AS = b.RelationsipToIndexClient 
+				and a.CurrentlyLivingWithIndexClient COLLATE Latin1_General_CI_AS = b.CurrentlyLivingWithIndexClient
+				and a.Age  = b.Age 
+				and a.CccNumber COLLATE Latin1_General_CI_AS = b.CccNumber 
+				and a.Gender COLLATE Latin1_General_CI_AS = b.Gender 
+				and a.FacilityLinkedTo COLLATE Latin1_General_CI_AS = b.FacilityLinkedTo 
+				and a.DateElicited  = b.DateElicited
+				and a.Dob  = b.Dob
+
 			)
 	WHEN NOT MATCHED THEN 
 		INSERT(FacilityName,SiteCode,PatientPk,HtsNumber,Emr,Project,PartnerPatientPk,KnowledgeOfHivStatus,PartnerPersonID,CccNumber,IpvScreeningOutcome,ScreenedForIpv,PnsConsent,RelationsipToIndexClient,LinkedToCare,MaritalStatus,PnsApproach,FacilityLinkedTo,Gender,CurrentlyLivingWithIndexClient,Age,DateElicited,Dob,LinkDateLinkedToCare) 
