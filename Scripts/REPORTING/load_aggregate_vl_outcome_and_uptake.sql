80870be040363a8ea255be7db6aa3766481bd9b4
cbrianbet <cbrianbet@outlook.com>
2023-02-14 22:53:16 +0300

    Update load_aggregate_vl_outcome_and_uptake.sql

--- Scripts/REPORTING/load_aggregate_vl_outcome_and_uptake.sql
+++ Scripts/REPORTING/load_aggregate_vl_outcome_and_uptake.sql
@@ -50,7 +50,7 @@ FROM
 	INNER JOIN NDWH.dbo.DimAgency a ON a.AgencyKey = it.AgencyKey
 	INNER JOIN NDWH.dbo.DimPatient pat ON pat.PatientKey = it.PatientKey
 	INNER JOIN NDWH.dbo.DimPartner p ON p.PartnerKey = it.PartnerKey
-	INNER JOIN NDWH.dbo.FactART art ON art.PatientKey = it.PatientKey 
+	LEFT JOIN NDWH.dbo.FactART art ON art.PatientKey = it.PatientKey 
 WHERE
 	IsTXCurr = 1 
 GROUP BY
