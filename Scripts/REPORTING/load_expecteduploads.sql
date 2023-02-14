cd07622fd95c051f0cffd8ec1f53fd86210cfced
cbrianbet <cbrianbet@outlook.com>
2023-02-15 00:51:40 +0300

    Create load_expecteduploads.sql

---
+++ Scripts/REPORTING/load_expecteduploads.sql
@@ -0,0 +1,49 @@
+IF OBJECT_ID(N'REPORTING.DBO.expected_uploads ', N'U') IS NOT NULL 
+	DROP TABLE REPORTING.DBO.expected_uploads ;
+
+BEGIN
+	with uploads as (
+		SELECT 
+			'CT' AS docket,
+			f.County AS county,
+			f.SubCounty AS subcounty,
+			SDP_Agency AS agency,
+			SDP AS partner,
+			COUNT(DISTINCT MFLCode) AS expected
+		FROM
+			NDWH.DBO.DimFacility f
+			INNER JOIN ODS.dbo.All_EMRSites a on a.MFL_Code = MFLCode
+		WHERE
+			(isCT = 1)
+		GROUP BY f.county, f.subCounty, SDP_Agency, SDP
+		UNION 
+		SELECT 
+			'HTS' AS docket,
+			h.County AS county,
+			h.subCounty AS subcounty,
+			SDP_Agency AS agency,
+			SDP AS partner,
+			COUNT(DISTINCT MFLCode) AS expected
+		FROM
+			NDWH.DBO.DimFacility h
+			INNER JOIN ODS.dbo.All_EMRSites a on a.MFL_Code = MFLCode
+		WHERE
+			(isHts = 1)
+		GROUP BY h.county, h.subCounty, SDP_Agency, SDP
+		UNION 
+		SELECT 
+			'PKV' AS docket,
+			p.county AS county,
+			p.subCounty AS subcounty,
+			SDP_Agency AS agency,
+			SDP AS partner,
+			COUNT(DISTINCT MFLCode) AS expected
+		FROM
+			NDWH.DBO.DimFacility p
+			INNER JOIN ODS.dbo.All_EMRSites a on a.MFL_Code = MFLCode
+		WHERE
+			(isPkv = 1)
+		GROUP BY p.county, p.subCounty, SDP_Agency, SDP
+	) 
+	select * into REPORTING.DBO.expected_uploads  from uploads
+END
