f64f35082e33d2d115d51882675ea68a9184cbb1
cbrianbet <cbrianbet@outlook.com>
2023-02-22 14:27:49 +0300

    correct dhis2 pull

--- Scripts/REPORTING/load_aggregate_hts_dhis2.sql
+++ Scripts/REPORTING/load_aggregate_hts_dhis2.sql
@@ -1,49 +1,46 @@
-SELECT 
-	Y.* 
-INTO REPORTING.dbo.FACT_CT_DHIS2
-FROM ( 
-	SELECT
-		id
-		,DHISOrgId
-		,SiteCode
-		,FacilityName
-		,CT.County
-		,CT.SubCounty
-		,Ward
-		,ReportMonth_Year
-		,Enrolled_Total
-		,StartedART_Total
-		,CurrentOnART_Total
-		,CTX_Total
-		,OnART_12Months
-		,NetCohort_12Months
-		,VLSuppression_12Months
-		,VLResultAvail_12Months
-		,createdAt
-		,updatedAt
-		,Start_ART_Under_1
-		,Start_ART_1_9
-		,Start_ART_10_14_M
-		,Start_ART_10_14_F
-		,Start_ART_15_19_M
-		,Start_ART_15_19_F
-		,Start_ART_20_24_M
-		,Start_ART_20_24_F
-		,Start_ART_25_Plus_M
-		,Start_ART_25_Plus_F
-		,On_ART_Under_1
-		,On_ART_1_9
-		,On_ART_10_14_M
-		,On_ART_10_14_F
-		,On_ART_15_19_M
-		,On_ART_15_19_F
-		,On_ART_20_24_M
-		,On_ART_20_24_F
-		,On_ART_25_Plus_M
-		,On_ART_25_Plus_F
-		,Sites.SDP
-		,Sites.[SDP Agency]
-	FROM All_Staging_2016_2.dbo.FACT_CT_DHIS2 CT
-	LEFT JOIN HIS_Implementation.dbo.ALL_EMRSites Sites on CT.SiteCode COLLATE Latin1_General_CI_AS=Sites.MFL_Code
-)Y 
-where SDP is not null
+IF OBJECT_ID(N'[REPORTING].[dbo].[AggregateFACT_HTS_DHIS2]', N'U') IS NOT NULL 
+	DROP TABLE [REPORTING].[dbo].[AggregateFACT_HTS_DHIS2];
+BEGIN
+	SELECT 
+		Y.* 
+	INTO REPORTING.dbo.AggregateFACT_HTS_DHIS2
+	FROM ( 
+		SELECT
+			[id]
+			,[DHISOrgId]
+			,[SiteCode]
+			,[FacilityName]
+			,CT.County
+			,CT.SubCounty
+			,[Ward]
+			,[ReportMonth_Year]
+			,[Tested_Total]
+			,[Positive_Total]
+			,[createdAt]
+			,[updatedAt]
+			,[Tested_1_9]
+			,[Tested_10_14_M]
+			,[Tested_10_14_F]
+			,[Tested_15_19_M]
+			,[Tested_15_19_F]
+			,[Tested_20_24_M]
+			,[Tested_20_24_F]
+			,[Tested_25_Plus_M]
+			,[Tested_25_Plus_F]
+			,[Positive_1_9]
+			,[Positive_10_14_M]
+			,[Positive_10_14_F]
+			,[Positive_15_19_M]
+			,[Positive_15_19_F]
+			,[Positive_20_24_M]
+			,[Positive_20_24_F]
+			,[Positive_25_Plus_M]
+			,[Positive_25_Plus_F]
+			,Sites.SDP PartnerName
+			,Sites.[SDP Agency] AgencyName
+		FROM NDWH.dbo.FACT_HTS_DHIS2 CT
+		LEFT JOIN HIS_Implementation.dbo.ALL_EMRSites Sites on CT.SiteCode COLLATE Latin1_General_CI_AS=Sites.MFL_Code
+	)Y 
+	WHERE PartnerName IS NOT NULL
+
+END
