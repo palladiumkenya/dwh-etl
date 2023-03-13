8d6497638ba811aab33a81036ebcd9ac2c3273c4
cbrianbet <cbrianbet@outlook.com>
2023-03-13 20:45:33 +0300

    Create load_aggregate_hts_tbscreening.sql

---
+++ Scripts/REPORTING/load_aggregate_hts_tbscreening.sql
@@ -0,0 +1,34 @@
+IF EXISTS(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'REPORTING.[dbo].[AggregateHTSTBscreening]') AND type in (N'U')) 
+TRUNCATE TABLE REPORTING.[dbo].[AggregateHTSTBscreening]
+GO
+
+INSERT INTO REPORTING.dbo.AggregateHTSTBscreening (MFLCode, FacilityName, County, SubCounty, PartnerName, AgencyName, Gender, AgeGroup,
+	tbScreening, year, month, MonthName, Tested, Positive, Linked
+)
+SELECT DISTINCT
+MFLCode,
+f.FacilityName,
+County,
+SubCounty,
+p.PartnerName,
+a.AgencyName,
+Gender,
+age.DATIMAgeGroup as AgeGroup,
+tbScreening,
+-- TBScreening_Grp,
+year,
+month,
+FORMAT(cast(date as date), 'MMMM') MonthName,
+Sum(Tested) Tested,
+Sum(Positive) Positive,
+Sum(Linked) Linked
+
+FROM NDWH.dbo.FactHTSClientTests hts
+LEFT join NDWH.dbo.DimFacility f on f.FacilityKey = hts.FacilityKey
+LEFT JOIN NDWH.dbo.DimAgency a on a.AgencyKey = hts.AgencyKey
+LEFT JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = hts.PatientKey
+LEFT join NDWH.dbo.DimAgeGroup age on age.AgeGroupKey=hts.AgeGroupKey
+LEFT JOIN NDWH.dbo.DimPartner p on p.PartnerKey = hts.PartnerKey
+LEFT JOIN NDWH.dbo.FactHTSClientLinkages link on link.PatientKey = hts.PatientKey
+LEFT JOIN NDWH.dbo.DimDate d on d.DateKey = hts.DateTestedKey
+GROUP BY MFLCode, f.FacilityName, County, SubCounty, p.PartnerName, a.AgencyName, Gender, age.DATIMAgeGroup, tbScreening, year, month, FORMAT(cast(date as date), 'MMMM')
