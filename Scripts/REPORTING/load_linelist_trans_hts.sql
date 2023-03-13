df5d32096a47908e905e333a248f872259fef08e
cbrianbet <cbrianbet@outlook.com>
2023-03-13 18:33:46 +0300

    Create load_linelist_trans_hts.sql

---
+++ Scripts/REPORTING/load_linelist_trans_hts.sql
@@ -0,0 +1,83 @@
+IF EXISTS(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'REPORTING.[dbo].[LineListTransHTS]') AND type in (N'U')) 
+TRUNCATE TABLE REPORTING.[dbo].[LineListTransHTS]
+GO
+
+INSERT INTO REPORTING.dbo.LineListTransHTS (MFLCode, EMR, FacilityName, County, SubCounty, PartnerName, AgencyName, Gender, AgeGroup,
+	PatientPKHash, 
+	TestDate, 
+	DOB, 
+	AgeAtTesting, 
+	EverTestedForHiv, 
+	MonthsSinceLastTest, 
+	ClientTestedAs, 
+	EntryPoint,
+	TestStrategy,
+	TestResult1,
+	TestResult2,
+	FinalTestResult,
+	PatientGivenResult,
+	TestType,
+	tbScreening,
+	ClientSelfTested,
+	CoupleDiscordant,
+	consent,
+	EnrollmentDate,
+	ReportedCCCNumber,
+	EncounterId,
+	project,
+	Tested,
+	Positive,
+	Linked,
+	MonthsLastTest,
+	TestedBefore,
+	MaritalStatus
+)
+SELECT DISTINCT
+MFLCode,
+EMR,
+f.FacilityName,
+County,
+SubCounty,
+p.PartnerName,
+a.AgencyName,
+Gender,
+age.DATIMAgeGroup as AgeGroup,
+PatientPKHash,
+d.Date TestDate,
+CAST(DOB as DATE) DOB,
+AgeAtTesting,
+EverTestedForHiv,
+MonthsSinceLastTest,
+ClientTestedAs,
+EntryPoint,
+TestStrategy,
+TestResult1,
+TestResult2,
+FinalTestResult,
+PatientGivenResult,
+TestType,
+tbScreening,
+ClientSelfTested,
+CoupleDiscordant,
+consent,
+e.date EnrollmentDate,
+hts.ReportedCCCNumber,
+EncounterId,
+project,
+Tested,
+Positive,
+Linked,
+MonthsLastTest,
+TestedBefore,
+MaritalStatus
+
+FROM NDWH.dbo.FactHTSClientTests hts
+INNER join NDWH.dbo.DimFacility f on f.FacilityKey = hts.FacilityKey
+INNER JOIN NDWH.dbo.DimAgency a on a.AgencyKey = hts.AgencyKey
+INNER JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = hts.PatientKey
+INNER join NDWH.dbo.DimAgeGroup age on age.AgeGroupKey=hts.AgeGroupKey
+INNER JOIN NDWH.dbo.DimPartner p on p.PartnerKey = hts.PartnerKey
+LEFT JOIN NDWH.dbo.FactHTSClientLinkages link on link.PatientKey = hts.PatientKey
+LEFT JOIN NDWH.dbo.DimDate e on e.DateKey = DateEnrolledKey
+LEFT JOIN NDWH.dbo.DimDate d on d.DateKey = hts.DateTestedKey
+WHERE  ( DATEDIFF ( MONTH, DOB, d.Date ) > 18 AND DATEDIFF ( MONTH, DOB, d.Date ) <= 1500 )
