afd76a18a1b4e4ce404fb7d3af8807bdbe692d44
cbrianbet <cbrianbet@outlook.com>
2023-03-14 13:58:18 +0300

    Update load_linelist_trans_hts.sql

--- Scripts/REPORTING/load_linelist_trans_hts.sql
+++ Scripts/REPORTING/load_linelist_trans_hts.sql
@@ -72,11 +72,11 @@ TestedBefore,
 MaritalStatus
 
 FROM NDWH.dbo.FactHTSClientTests hts
-INNER join NDWH.dbo.DimFacility f on f.FacilityKey = hts.FacilityKey
-INNER JOIN NDWH.dbo.DimAgency a on a.AgencyKey = hts.AgencyKey
-INNER JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = hts.PatientKey
-INNER join NDWH.dbo.DimAgeGroup age on age.AgeGroupKey=hts.AgeGroupKey
-INNER JOIN NDWH.dbo.DimPartner p on p.PartnerKey = hts.PartnerKey
+LEFT JOIN NDWH.dbo.DimFacility f on f.FacilityKey = hts.FacilityKey
+LEFT JOIN NDWH.dbo.DimAgency a on a.AgencyKey = hts.AgencyKey
+LEFT JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = hts.PatientKey
+LEFT JOIN NDWH.dbo.DimAgeGroup age on age.AgeGroupKey=hts.AgeGroupKey
+LEFT JOIN NDWH.dbo.DimPartner p on p.PartnerKey = hts.PartnerKey
 LEFT JOIN NDWH.dbo.FactHTSClientLinkages link on link.PatientKey = hts.PatientKey
 LEFT JOIN NDWH.dbo.DimDate e on e.DateKey = DateEnrolledKey
 LEFT JOIN NDWH.dbo.DimDate d on d.DateKey = hts.DateTestedKey
