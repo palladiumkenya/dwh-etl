21b11893d7412e5275734a978ec79b0652cea1d2
cbrianbet <cbrianbet@outlook.com>
2023-03-15 08:59:09 +0300

    Push latest SSIS changes

---
+++ Scripts/ODS/data_quality/CT_cleaning_scripts/clean_CT_patient_status.sql
@@ -0,0 +1,29 @@
+-- clean ExitDate
+UPDATE [ODS].[DBO].[CT_PatientStatus]
+    SET ExitDate = CAST('1900-01-01' AS DATE)
+WHERE ExitDate < CAST('2004-01-01' AS DATE) OR ExitDate > GETDATE()
+
+GO
+
+
+-- clean EMR 
+UPDATE [ODS].[DBO].[CT_PatientStatus]
+    SET Emr = CASE
+                WHEN Emr = 'Ampath AMRS' THEN 'AMRS'
+            END
+WHERE Emr = 'Ampath AMRS'
+
+GO
+
+
+-- clean Project
+UPDATE [ODS].[DBO].[CT_PatientStatus]
+    SET Project = CASE
+                WHEN Project IN ('Ampathplus') THEN  'Ampath Plus'
+                WHEN Project IN ('UCSF Clinical Kisumu', 'CHAP Uzima', 'DREAM Kenya Trusts', 'IRDO')  THEN 'Kenya HMIS II'
+            END
+WHERE Project IN ('Ampathplus', 'AMPATH', 'UCSF Clinical Kisumu', 'CHAP Uzima', 'DREAM Kenya Trusts', 'IRDO')
+
+GO
+
+
