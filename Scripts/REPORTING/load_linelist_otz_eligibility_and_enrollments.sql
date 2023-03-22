17ad4e404b35eb958b4bf2b2f8606af862ed19af
cbrianbet <cbrianbet@outlook.com>
2023-03-22 16:57:49 +0300

    Correction to otz tables

--- Scripts/REPORTING/load_linelist_otz_eligibility_and_enrollments.sql
+++ Scripts/REPORTING/load_linelist_otz_eligibility_and_enrollments.sql
@@ -2,9 +2,40 @@ IF  EXISTS (SELECT * FROM REPORTING.sys.objects WHERE object_id = OBJECT_ID(N'[R
 	TRUNCATE TABLE [REPORTING].[dbo].[LineListOTZEligibilityAndEnrollments]
 GO
 --- A linelist of ALHIV patients (Enrolled + Not Enrolled to OTZ)
-INSERT INTO REPORTING.dbo.LineListOTZEligibilityAndEnrollments
+INSERT INTO REPORTING.dbo.LineListOTZEligibilityAndEnrollments (
+	MFLCode,
+	FacilityName,
+	County,
+	SubCounty,
+	PartnerName,
+	AgencyName,
+	Gender,
+	AgeGroup,
+	OTZEnrollmentDateKey,
+	LastVisitDateKey,
+	TransitionAttritionReason,
+	TransferInStatus,
+	CompletedTraining,
+	ModulesPreviouslyCovered,
+	ModulesCompletedToday_OTZ_Orientation,
+	ModulesCompletedToday_OTZ_Participation,
+	ModulesCompletedToday_OTZ_Leadership,
+	ModulesCompletedToday_OTZ_MakingDecisions,
+	ModulesCompletedToday_OTZ_Transition,
+	ModulesCompletedToday_OTZ_TreatmentLiteracy,
+	ModulesCompletedToday_OTZ_SRH,
+	ModulesCompletedToday_OTZ_Beyond,
+	FirstVL,
+	LastVL,
+	EligibleVL,
+	Last12MonthVLResults,
+	Last12MVLResult,
+	Last12MonthVL,
+	Eligible,
+	Enrolled
+)
 SELECT DISTINCT
-				MFLCode,
+	MFLCode,
 	f.FacilityName,
 	County,
 	SubCounty,
@@ -41,15 +72,13 @@ SELECT DISTINCT
 			ELSE NULL END  
 		END AS Last12MVLResult,
 	vl.Last12MonthVL as Last12MonthVL,
-	CASE
+	COUNT(CASE
 	WHEN art.PatientKey is not null THEN 1
-	ELSE 0 END as Eligible,
-	CASE
+	ELSE 0 END) as Eligible,
+	COUNT(CASE
 	WHEN otz.PatientKey is not null THEN 1
-	ELSE 0 END as Enrolled
+	ELSE NULL END) as Enrolled
 
-	
-	
 FROM NDWH.dbo.FACTART art
 
 INNER JOIN NDWH.dbo.DimAgeGroup age ON age.AgeGroupKey= art.AgeGroupKey
@@ -58,7 +87,45 @@ INNER JOIN NDWH.dbo.DimAgency a ON a.AgencyKey = art.AgencyKey
 INNER JOIN NDWH.dbo.DimPatient pat ON pat.PatientKey = art.PatientKey
 INNER JOIN NDWH.dbo.DimPartner p ON p.PartnerKey = art.PartnerKey
 LEFT JOIN NDWH.dbo.FactViralLoads vl ON vl.PatientKey = art.PatientKey AND vl.PatientKey IS NOT NULL 
-FULL OUTER JOIN NDWH.dbo.FactOTZ otz ON otz.PatientKey = art.PatientKey
-WHERE
-		age.Age BETWEEN 10 AND 24 AND IsTXCurr = 1
+FULL OUTER JOIN NDWH.dbo.FactOTZ otz on otz.PatientKey = art.PatientKey
+
+WHERE age.Age BETWEEN 10 AND 24  AND IsTXCurr = 1
+
+GROUP BY MFLCode,
+	f.FacilityName,
+	County,
+	SubCounty,
+	p.PartnerName,
+	a.AgencyName,
+	Gender,
+	age.DATIMAgeGroup,
+	otz.OTZEnrollmentDateKey,
+	LastVisitDateKey,
+	TransitionAttritionReason,
+	TransferInStatus,
+	case when otz.ModulesPreviouslyCovered is not null then 1 else 0 end,
+	ModulesPreviouslyCovered,
+	ModulesCompletedToday_OTZ_Orientation,
+	ModulesCompletedToday_OTZ_Participation,
+	ModulesCompletedToday_OTZ_Leadership,
+	ModulesCompletedToday_OTZ_MakingDecisions,
+	ModulesCompletedToday_OTZ_Transition,
+	ModulesCompletedToday_OTZ_TreatmentLiteracy,
+	ModulesCompletedToday_OTZ_SRH,
+	ModulesCompletedToday_OTZ_Beyond,
+	FirstVL,
+	LastVL,
+	vl.EligibleVL,
+	Last12MonthVLResults,
+	CASE 
+		WHEN ISNUMERIC(vl.Last12MonthVLResults) = 1 
+			THEN CASE WHEN CAST(Replace(vl.Last12MonthVLResults,',','')AS FLOAT) < 400.00 THEN 'VL' 
+			WHEN CAST(Replace(vl.Last12MonthVLResults,',','')AS FLOAT) between 400.00 and 1000.00 THEN 'LVL'
+			WHEN CAST(Replace(vl.Last12MonthVLResults,',','') AS FLOAT) > 1000.00 THEN 'HVL'
+			ELSE NULL END 
+		ELSE 
+			CASE WHEN vl.Last12MonthVLResults  IN ('Undetectable','NOT DETECTED','0 copies/ml','LDL','Less than Low Detectable Level') THEN 'VL' 
+			ELSE NULL END  
+		END,
+	vl.Last12MonthVL
 GO
