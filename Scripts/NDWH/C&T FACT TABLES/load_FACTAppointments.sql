f4a96879befa36a1393a24c1b37d421bd73d9050
Marymary-Dev <marywambu@gmail.com>
2023-04-12 11:07:55 +0300

    Reorder column for agegroup and NumofPatients

--- Scripts/NDWH/C&T FACT TABLES/load_FACTAppointments.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FACTAppointments.sql
@@ -1,66 +1,35 @@
+IF OBJECT_ID(N'[REPORTING].[dbo].AggregateAppointments', N'U') IS NOT NULL 		
+	truncate table [REPORTING].[dbo].AggregateAppointments
+GO
 
-IF OBJECT_ID(N'[NDWH].[dbo].[FACTAppointments]', N'U') IS NOT NULL 
-	DROP TABLE [NDWH].[dbo].[FACTAppointments];
-BEGIN
-with MFL_partner_agency_combination as (
-	select 
-		distinct MFL_Code,
-		SDP,
-	  SDP_Agency as Agency
-	from ODS.dbo.All_EMRSites 
-),
+Insert into [REPORTING].[dbo].AggregateAppointments
+select 
+    facility.MFLCode,
+    facility.FacilityName,
+    facility.SubCounty,
+    facility.County,
+    partner.PartnerName,
+    agency.AgencyName,
+    patient.Gender,
+    AppointmentStatus,
+    age_group.DATIMAgeGroup,
+    count(*) NumOfPatients,
+    AsOfDate
+from NDWH.dbo.FACTAppointments as apt
+left join NDWH.dbo.DimFacility as facility on facility.FacilityKey = apt.FacilityKey
+left join NDWH.dbo.DimPartner as partner on partner.PartnerKey = apt.PartnerKey
+left join NDWH.dbo.DimPatient as patient on patient.PatientKey = apt.PatientKey
+left join NDWH.dbo.DimAgency as agency on agency.AgencyKey = apt.AgencyKey
+left join NDWH.dbo.DimAgeGroup as age_group on age_group.AgeGroupKey = DATEDIFF(YY,patient.DOB,apt.AsOfDate)
 
-   Patient As ( Select    
-      PatientPKHash,
-      PatientIDHash,
-      MFlCode,
-      ExpectedNextAppointmentDate,
-      LastEncounterDate,
-      NextAppointmentDate,
-      DiffExpectedTCADateLastEncounter,
-      AppointmentStatus,
-      AsOfDate,
-      StartARTDate,
-      ARTDurationMonths,
-      DateRecieved
-
-from 
-ODS.dbo.[HistoricalAppointmentStatus] Patient
-
-   )
-
-   Select 
-            Factkey = IDENTITY(INT, 1, 1),
-            pat.PatientKey,
-            fac.FacilityKey,
-            partner.PartnerKey,
-            agency.AgencyKey,
-            StartARTDate.Date As StartARTDateKey,
-            ExpectedNextAppointmentDate,
-            Patient.LastEncounterDate,
-            Patient.NextAppointmentDate,
-            DiffExpectedTCADateLastEncounter,
-            AppointmentStatus,
-            AsOfDate,
-            Patient.StartARTDate,
-            ARTDurationMonths,
-            DateRecieved,
-            cast(getdate() as date) as LoadDate
-INTO NDWH.dbo.FACTAppointments
-from  Patient
-left join NDWH.dbo.DimPatient as Pat on pat.PatientPKHash=Patient.PatientPkHash and Pat.SiteCode=Patient.MFLCode
-left join NDWH.dbo.Dimfacility fac on fac.MFLCode=Patient.MFLCode
-left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code  = Patient.MFLCode 
-left join NDWH.dbo.DimPartner as partner on partner.PartnerName = MFL_partner_agency_combination.SDP 
-left join NDWH.dbo.DimDate as StartARTDate on StartARTDate.Date= Patient.StartARTDate
-left join NDWH.dbo.DimAgency as agency on agency.AgencyName = MFL_partner_agency_combination.Agency
-
-alter table NDWH.dbo.FACTAppointments add primary key(FactKey)
-
-
-END
-
-
-
-
-
+group by 
+    facility.MFLCode,
+    facility.FacilityName,
+    facility.SubCounty,
+    facility.County,
+    partner.PartnerName,
+    agency.AgencyName,
+    patient.Gender,
+    AppointmentStatus,
+    DATIMAgeGroup,
+    AsOfDate
