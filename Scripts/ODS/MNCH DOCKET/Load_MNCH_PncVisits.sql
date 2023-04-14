19d7e0e4736ce2d544b6b851d7054edc0a007103
cbrianbet <cbrianbet@outlook.com>
2023-04-14 22:07:41 +0300

    pull and push

--- Scripts/ODS/MNCH DOCKET/Load_MNCH_PncVisits.sql
+++ Scripts/ODS/MNCH DOCKET/Load_MNCH_PncVisits.sql
@@ -37,4 +37,17 @@ BEGIN
 					WHEN MATCHED THEN
 						UPDATE SET 
 							a.ClinicalNotes	 =b.ClinicalNotes;
+
+				with cte AS (
+						Select
+						Sitecode,
+						PatientPK,
+						VisitDate,
+
+						 ROW_NUMBER() OVER (PARTITION BY PatientPK,Sitecode,VisitDate ORDER BY
+						PatientPK,Sitecode,VisitDate) Row_Num
+						FROM  [ODS].[dbo].[MNCH_PncVisits](NoLock)
+						)
+						delete from cte 
+						Where Row_Num >1 ;
 END
