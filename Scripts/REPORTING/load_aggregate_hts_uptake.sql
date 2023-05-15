98ef8fa278b7ecbd3e871ba049ec3b325342a5c3
Nobert Mumo <mumongungu@gmail.com>
2023-05-15 12:08:06 +0300

    fixing hts uptake aggregates table

--- Scripts/REPORTING/load_aggregate_hts_uptake.sql
+++ Scripts/REPORTING/load_aggregate_hts_uptake.sql
@@ -5,23 +5,23 @@ GO
 INSERT INTO REPORTING.dbo.AggregateHTSUptake (MFLCode, FacilityName, County, SubCounty, PartnerName, AgencyName, Gender, AgeGroup,
 	TestedBefore, year, month, MonthName, Tested, Positive, Linked
 )
-SELECT DISTINCT
-MFLCode,
-f.FacilityName,
-County,
-SubCounty,
-p.PartnerName,
-a.AgencyName,
-Gender,
-age.DATIMAgeGroup as AgeGroup,
-TestedBefore,
-year,
-month,
-FORMAT(cast(date as date), 'MMMM') MonthName,
-Sum(Tested) Tested,
-Sum(Positive) Positive,
-Sum(Linked) Linked
-
+SELECT 
+	DISTINCT
+	MFLCode,
+	f.FacilityName,
+	County,
+	SubCounty,
+	p.PartnerName,
+	a.AgencyName,
+	Gender,
+	age.DATIMAgeGroup as AgeGroup,
+	TestedBefore,
+	year,
+	month,
+	FORMAT(cast(date as date), 'MMMM') as MonthName,
+	Sum(Tested) as Tested,
+	Sum(Positive) as Positive,
+	Sum(Linked) as Linked
 FROM NDWH.dbo.FactHTSClientTests hts
 LEFT join NDWH.dbo.DimFacility f on f.FacilityKey = hts.FacilityKey
 LEFT JOIN NDWH.dbo.DimAgency a on a.AgencyKey = hts.AgencyKey
@@ -30,4 +30,17 @@ LEFT join NDWH.dbo.DimAgeGroup age on age.AgeGroupKey=hts.AgeGroupKey
 LEFT JOIN NDWH.dbo.DimPartner p on p.PartnerKey = hts.PartnerKey
 LEFT JOIN NDWH.dbo.FactHTSClientLinkages link on link.PatientKey = hts.PatientKey
 LEFT JOIN NDWH.dbo.DimDate d on d.DateKey = hts.DateTestedKey
-GROUP BY MFLCode, f.FacilityName, County, SubCounty, p.PartnerName, a.AgencyName, Gender, age.DATIMAgeGroup, TestedBefore, year, month, FORMAT(cast(date as date), 'MMMM')
+WHERE TestType in ('Initial Test', 'Initial')
+GROUP BY 
+	MFLCode, 
+	f.FacilityName,
+	County, 
+	SubCounty, 
+	p.PartnerName, 
+	a.AgencyName, 
+	Gender, 
+	age.DATIMAgeGroup, 
+	TestedBefore, 
+	year, 
+	month, 
+	FORMAT(cast(date as date), 'MMMM')
