823465e0b7ff8caea598b9eac4322dab75e994b4
Marymary-Dev <marywambu@gmail.com>
2023-07-31 16:52:20 +0300

    Set To Nulls where the dates are less than the year 2004

--- Scripts/ODS/data_quality/CT_cleaning_scripts/clean_CT_patient_status.sql
+++ Scripts/ODS/data_quality/CT_cleaning_scripts/clean_CT_patient_status.sql
@@ -4,7 +4,22 @@ UPDATE [ODS].[DBO].[CT_PatientStatus]
 WHERE ExitDate < CAST('2004-01-01' AS DATE) OR ExitDate > GETDATE()
 
 GO
+--Clean Re-Enrollment Date
+UPDATE [ODS].[DBO].[CT_PatientStatus]
+    SET ReEnrollmentDate = NULL
+WHERE ReEnrollmentDate < CAST('2004-01-01' AS DATE) OR ReEnrollmentDate > GETDATE()
 
+GO
+---Clean To Verified date
+UPDATE [ODS].[DBO].[CT_PatientStatus]
+    SET TOVerifiedDate = NULL
+WHERE TOVerifiedDate < CAST('2004-01-01' AS DATE) 
+
+GO 
+--Clean Effective Discontinuation date 
+UPDATE [ODS].[DBO].[CT_PatientStatus]
+    SET EffectiveDiscontinuationDate = NULL
+WHERE EffectiveDiscontinuationDate < CAST('2004-01-01' AS DATE) 
 
 -- clean EMR 
 UPDATE [ODS].[DBO].[CT_PatientStatus]
