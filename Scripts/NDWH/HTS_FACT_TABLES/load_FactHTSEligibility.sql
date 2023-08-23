c595aa2acc288bcfb747aa59fb65e16e120e8db7
Mumo Ngungu <nobert.mumo@thepalladiumgroup.com>
2023-08-23 12:37:29 +0300

    correct logic for Eligibility

--- Scripts/NDWH/HTS_FACT_TABLES/load_FactHTSEligibility.sql
+++ Scripts/NDWH/HTS_FACT_TABLES/load_FactHTSEligibility.sql
@@ -25,7 +25,7 @@ BEGIN
             cast(VisitDate as date) as VisitDate,
             cast (DateTestedSelf as date) As DateTestedSelf,
             StartedOnART,
-            CONVERT(NVARCHAR(64), HASHBYTES('SHA2_256', CAST(CCCNumber as NVARCHAR(36))), 2) As CCCNumber,
+            CONVERT(NVARCHAR(64), HASHBYTES('SHA2_256', CAST(CCCNumber as NVARCHAR(36))), 2) As CCCNumberHash,
             EverHadSex,
             SexuallyActive,
             NewPartner,
@@ -70,22 +70,76 @@ BEGIN
             ReceivedServices,
             TypeGBV,
 			HIVRiskCategory,
-			HtsRiskScore
+			HtsRiskScore,
+            ReasonRefferredForTesting
         from ODS.dbo.HTS_EligibilityExtract
     )
     select 
         Factkey = IDENTITY(INT, 1, 1),
-		source_data.*,
         patient.PatientKey,
         facility.FacilityKey,
         partner.PartnerKey,
         agency.AgencyKey,
         VisitDate.DateKey As VisitDateKey,
         DateTestedSelf.DateKey as DateTestedSelfKey,
+        source_data.Department,
+        source_data.IsHealthWorker,
+        source_data.RelationshipWithContact,
+        source_data.TestedHIVBefore,
+        source_data.WhoPErformedTest,
+        source_data.ResultOfHIV,
+        source_data.StartedOnART,
+        source_data.CCCNumberHash,
+        source_data.EverHadSex,
+        source_data.SexuallyActive,
+        source_data.NewPartner,
+        source_data.PartnerHIVStatus,
+        source_data.CoupleDiscordant,
+        source_data.MultiplePartners,
+        source_data.NumberOfPartners,
+        source_data.AlcoholSex,
+        source_data.MoneySex,
+        source_data.CondomBurst,
+        source_data.UnknownStatusPartner,
+        source_data.KnownStatusPartner,
+        source_data.Pregnant,
+        source_data.BreastfeedingMother,
+        source_data.ExperiencedGBV,
+        source_data.ContactWithTBCase,
+        source_data.Lethargy,
+        source_data.EverOnPrep,
+        source_data.CurrentlyOnPep,
+        source_data.EverHadSTI,
+        source_data.CurrentlyHasSTI,
+        source_data.EverHadTB,
+        source_data.SharedNeedle,
+        source_data.NeedleStickInjuries,
+        source_data.TraditionalProcedures,
+        source_data.ChildReasonsForIneligibility,
+        source_data.EligibleForTest,
+        source_data.ReasonsforIneligibility,
+        source_data.specificReasonForIneligibility,
+        source_data.Cough,
+        cast (DateTestedProvider as date) As DateTestedProvider,
+        source_data.Fever,
+        source_data.MothersStatus,
+        source_data.NightSweats,
+        source_data.ReferredForTesting,
+        source_data.ResultOfHIVSelf,
+        source_data.ScreenedTB,
+        source_data.TBStatus,
+        source_data.WeightLoss,
+        source_data.AssessmentOutcome,
+        source_data.ForcedSex,
+        source_data.ReceivedServices,
+        source_data.TypeGBV,
+        source_data.HIVRiskCategory,
+        source_data.HtsRiskScore,
+        source_data.ReasonRefferredForTesting,
         cast(getdate() as date) as LoadDate
     into NDWH.dbo.FactHTSEligibilityextract
     from source_data
-    left join NDWH.dbo.DimPatient as patient on patient.PatientPKHash = convert(nvarchar(64), hashbytes('SHA2_256', cast(source_data.PatientPK as nvarchar(36))), 2)
+    left join NDWH.dbo.DimPatient as patient on patient.PatientPKHash = source_data.PatientPKHash
         and patient.SiteCode = source_data.SiteCode
     left join NDWH.dbo.DimFacility as facility on facility.MFLCode = source_data.SiteCode
     left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = source_data.SiteCode
@@ -94,9 +148,6 @@ BEGIN
     left join NDWH.dbo.DimDate as VisitDate on VisitDate.Date = source_data.VisitDate
     left join NDWH.dbo.DimDate as DateTestedSelf on DateTestedSelf.Date = source_data.DateTestedSelf
     left join NDWH.dbo.DimDate as DateTestedProvider on DateTestedProvider.Date = source_data.DateTestedProvider
-    left join NDWH.dbo.DimPatient as patient on patient.PatientPKHash = source_data.PatientPKHash
-        and patient.SiteCode = source_data.SiteCode
-
 
 
     alter table NDWH.dbo.FactHTSEligibilityextract add primary key(FactKey);
