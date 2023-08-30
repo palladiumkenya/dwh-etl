219b02817bbf719fca518ff6bd6349e22c5a6cab
cbrianbet <cbrianbet@outlook.com>
2023-08-30 08:29:58 -0700

    Update load_linelist_ovc_enrollment.sql

--- Scripts/REPORTING/load_linelist_ovc_enrollment.sql
+++ Scripts/REPORTING/load_linelist_ovc_enrollment.sql
@@ -44,19 +44,19 @@ SELECT
     CAST(GETDATE() AS DATE) AS LoadDate 
 INTO [REPORTING].[dbo].LineListOVCEnrollments
 FROM [NDWH].[dbo].[FactOVC] it
-INNER JOIN NDWH.dbo.DimDate enrld on enrld.DateKey = it.OVCEnrollmentDateKey
-INNER join NDWH.dbo.DimFacility f on f.FacilityKey = it.FacilityKey
-INNER JOIN NDWH.dbo.DimAgency a on a.AgencyKey = it.AgencyKey
-INNER JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = it.PatientKey
-INNER JOIN NDWH.dbo.DimPartner p on p.PartnerKey = it.PartnerKey
-INNER JOIN NDWH.dbo.FactART art on art.PatientKey = it.PatientKey
-INNER JOIN NDWH.dbo.FactViralLoads vl on vl.PatientKey = it.PatientKey
-INNER join NDWH.dbo.DimAgeGroup g on g.Age = AgeLastVisit
+LEFT JOIN NDWH.dbo.DimDate enrld on enrld.DateKey = it.OVCEnrollmentDateKey
+LEFT join NDWH.dbo.DimFacility f on f.FacilityKey = it.FacilityKey
+LEFT JOIN NDWH.dbo.DimAgency a on a.AgencyKey = it.AgencyKey
+LEFT JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = it.PatientKey
+LEFT JOIN NDWH.dbo.DimPartner p on p.PartnerKey = it.PartnerKey
+LEFT JOIN NDWH.dbo.FactART art on art.PatientKey = it.PatientKey
+LEFT JOIN NDWH.dbo.FactViralLoads vl on vl.PatientKey = it.PatientKey
+LEFT join NDWH.dbo.DimAgeGroup g on g.Age = AgeLastVisit
 LEFT JOIN NDWH.dbo.DimDate exd on exd.DateKey = it.OVCExitDateKey
 LEFT JOIN NDWH.dbo.DimDate lvd on lvd.DateKey = vl.LastVLDateKey
 LEFT JOIN NDWH.dbo.DimDate fvd on fvd.DateKey = vl.FirstVLDateKey
 LEFT JOIN NDWH.dbo.DimDate validvl on validvl.DateKey = vl.ValidVLDateKey
 LEFT JOIN NDWH.dbo.DimRelationshipWithPatient rp on rp.RelationshipWithPatientKey = it.RelationshipWithPatientKey
-INNER JOIN NDWH.dbo.DimARTOutcome ao on ao.ARTOutcomeKey = art.ARTOutcomeKey
+LEFT JOIN NDWH.dbo.DimARTOutcome ao on ao.ARTOutcomeKey = art.ARTOutcomeKey
 LEFT JOIN NDWH.dbo.FactLatestObs lo on lo.PatientKey = it.PatientKey
-where art.AgeLastVisit between 0 and 17 and OVCExitReason is null
+where art.AgeLastVisit between 0 and 17 and OVCExitReason is null and IsTXCurr =1
