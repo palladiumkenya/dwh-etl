44df71465c66f48bd41fd849288fb797051fb057
Gikura <mary.gikura@thepalladiumgroup.com>
2023-09-18 20:15:08 +0300

    Adding Prep linelist

---
+++ Scripts/REPORTING/load_Linelist_Prep.sql
@@ -0,0 +1,36 @@
+
+IF EXISTS(SELECT * FROM REPORTING.sys.objects WHERE object_id = OBJECT_ID(N'REPORTING.dbo.LinelistPrep') AND type in (N'U')) 
+    DROP TABLE REPORTING.dbo.LinelistPrep
+GO
+
+WITH prepCascade AS  (
+	SELECT DISTINCT 
+        PatientPKHash,
+        MFLCode,		
+        f.FacilityName,
+        County,
+        SubCounty,
+        p.PartnerName,
+        a.AgencyName,
+        pat.Gender,
+        age.DATIMAgeGroup as AgeGroup,
+        ass.month AssessmentMonth,
+        ass.year AssessmentYear,
+        EOMONTH(ass.Date) as AsofDate,
+        EligiblePrep,
+        ScreenedPrep,
+    CAST(GETDATE() AS DATE) AS LoadDate 
+ 
+    FROM NDWH.dbo.FactPrepAssessments prep
+    
+	LEFT JOIN NDWH.dbo.DimFacility f on f.FacilityKey = prep.FacilityKey
+	LEFT JOIN NDWH.dbo.DimAgency a on a.AgencyKey = prep.AgencyKey
+	LEFT JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = prep.PatientKey
+	LEFT JOIN NDWH.dbo.DimAgeGroup age on age.AgeGroupKey=prep.AgeGroupKey
+	LEFT JOIN NDWH.dbo.DimPartner p on p.PartnerKey = prep.PartnerKey
+	LEFT JOIN NDWH.dbo.DimDate ass ON ass.DateKey = AssessmentVisitDateKey 
+	
+)
+Select * 
+  INTO REPORTING.dbo.LinelistPrep
+  from prepCascade
