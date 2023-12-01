823efb2d8f0dcf9a832c63c132bfb86b519108ba
Mumo Ngungu <nobert.mumo@thepalladiumgroup.com>
2023-12-01 10:04:10 +0300

    fixing DSD models aggregates to be same as linelist fact ART

--- Scripts/REPORTING/load_aggregate_dsd.sql
+++ Scripts/REPORTING/load_aggregate_dsd.sql
@@ -18,14 +18,25 @@ SELECT DISTINCT
     COUNT(StabilityAssessment) AS Stability,
     Sum(pat.isTXCurr) As TXCurr,
     cast(getdate() as date) as LoadDate
-INTO REPORTING.dbo.AggregateDSD 
-FROM NDWH.dbo.FactLatestObs lob
-INNER JOIN NDWH.dbo.DimAgeGroup age on age.AgeGroupKey = lob.AgeGroupKey
-INNER JOIN NDWH.dbo.DimFacility f on f.FacilityKey = lob.FacilityKey
-INNER JOIN NDWH.dbo.DimAgency a on a.AgencyKey = lob.AgencyKey
-INNER JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = lob.PatientKey
-INNER JOIN NDWH.dbo.DimPartner p on p.PartnerKey = lob.PartnerKey
-WHERE pat.isTXCurr = 1
-GROUP BY MFLCode, f.FacilityName, County, SubCounty, p.PartnerName, a.AgencyName, Gender, age.DATIMAgeGroup, StabilityAssessment,DifferentiatedCare
+INTO [REPORTING].[dbo].[AggregateDSD]
+FROM NDWH.dbo.FactART as art
+LEFT JOIN NDWH.dbo.FactLatestObs as lob on lob.Patientkey = art.PatientKey
+LEFT JOIN NDWH.dbo.DimAgeGroup age on age.AgeGroupKey = art.AgeGroupKey
+LEFT JOIN NDWH.dbo.DimFacility f on f.FacilityKey = art.FacilityKey
+LEFT JOIN NDWH.dbo.DimAgency a on a.AgencyKey = art.AgencyKey
+LEFT JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = art.PatientKey
+LEFT JOIN NDWH.dbo.DimPartner p on p.PartnerKey = art.PartnerKey
+WHERE pat.IsTXCurr = 1
+GROUP BY 
+    MFLCode, 
+    f.FacilityName, 
+    County, 
+    SubCounty, 
+    p.PartnerName,
+    a.AgencyName, 
+    Gender, 
+    age.DATIMAgeGroup, 
+    StabilityAssessment,
+    DifferentiatedCare
 
 GO
