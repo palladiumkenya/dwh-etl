7da48156386266ce4f4b6b14faa4eb6e3719b448
cbrianbet <cbrianbet@outlook.com>
2023-12-06 06:28:51 -0800

    Updated ValidVLResultCategory Limits
    50 Copies was excluded in previous categorization

--- Scripts/NDWH/C&T FACT TABLES/load_FactViralLoad.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactViralLoad.sql
@@ -88,7 +88,7 @@ BEGIN
 				case 
 					when cast(replace(TestResult,',','') AS float) >= 1000.00 then '>1000' 
 					when cast(replace(TestResult,',','') as float) between 200.00 and 999.00  then '200-999'
-					when cast(replace(TestResult,',','') as float) between 51.00 and 199.00 then '51-199'
+					when cast(replace(TestResult,',','') as float) between 50.00 and 199.00 then '51-199'
 					when cast(replace(TestResult,',','') as float) < 50 then '<50'
 				end 
 			else 
@@ -121,7 +121,7 @@ BEGIN
 					case 
 						when cast(replace(TestResult,',','') AS float) >= 1000.00 then '>1000' 
 						when cast(replace(TestResult,',','') as float) between 200.00 and 999.00  then '200-999'
-						when cast(replace(TestResult,',','') as float) between 51.00 and 199.00 then '51-199'
+						when cast(replace(TestResult,',','') as float) between 50.00 and 199.00 then '51-199'
 						when cast(replace(TestResult,',','') as float) < 50 then '<50'
 					end 
 				else 
@@ -134,7 +134,7 @@ BEGIN
 					case 
 						when cast(replace(TestResult,',','') AS float) >= 1000.00 then 'UNSUPPRESSED' 
 						when cast(replace(TestResult,',','') as float) between 200.00 and 999.00  then 'High Risk LLV '
-						when cast(replace(TestResult,',','') as float) between 51.00 and 199.00 then 'Low Risk LLV'
+						when cast(replace(TestResult,',','') as float) between 50.00 and 199.00 then 'Low Risk LLV'
 						when cast(replace(TestResult,',','') as float) < 50 then 'LDL'
 					end
 			else
