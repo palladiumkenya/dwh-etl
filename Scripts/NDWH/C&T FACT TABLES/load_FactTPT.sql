576d8ad882d2141b3d7dafd3a922943e522b5b83
Marymary-dev <marywambu@gmail.com>
2023-12-15 11:41:40 +0300

    Adding reporting month

--- Scripts/NDWH/C&T FACT TABLES/load_FactTPT.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactTPT.sql
@@ -1,106 +1,201 @@
-IF OBJECT_ID(N'[NDWH].[dbo].[FactTPT]', N'U') IS NOT NULL 
-	DROP TABLE [NDWH].[dbo].[FactTPT];
-BEGIN
-with MFL_partner_agency_combination as (
-	select 
-		distinct MFL_Code,
-		SDP,
-	    SDP_Agency  as Agency
-	from ODS.dbo.All_EMRSites
-),
-distinct_patients as (
-    select 
-        distinct PatientPk,
-        SiteCode
-    from  ODS.dbo.CT_IPT
-),
-date_started_TB_treatment as (
-    select 
-        distinct PatientPK,
-        SiteCode,
-        cast(TBRxStartDate as Date) as StartTBTreatmentDate 
-    from ODS.dbo.CT_IPT
-    where TBRxStartDate is not null
-),
-patient_TB_Diagnosis as (
-    select 
-        PatientPk,
-        SiteCode, 
-        min(ReportedbyDate) as TBDiagnosisDate
-    from ODS.dbo.CT_PatientLabs 
-    where (TestName like '%TB%' or TestName like '%Sput%' or TestName like '%TB%' or TestName like '%Tuber%')
-    and  TestName <> 'SputumGramStain'
-    and (TestResult like '%Positive%' or TestResult like '%HIGH%' or TestResult like '%+%')
-    group by  
-        PatientID,
-        PatientPk,
-        SiteCode
-),
-ipt_visits_ordered as (
-    select 
-        row_number() over (partition by PatientID, SiteCode, PatientPK order by VisitDate desc) as rank,
-        PatientID,
-        PatientPK,
-        SiteCode,
-        OnIPT,
-        OnTBDrugs
-    from ODS.dbo.CT_IPT
-),
-latest_visit as (
-    select 
-        *
-    from ipt_visits_ordered 
-    where rank = 1
-),
-combined_ipt_data as (
-    select
-       distinct_patients.PatientPK,
-       patient.PatientPKHash,
-       distinct_patients.SiteCode,
-       date_started_TB_treatment.StartTBTreatmentDate,
-       patient_TB_Diagnosis.TBDiagnosisDate,
-       latest_visit.OnIPT,
-        case 
-            when latest_visit.OnTBDrugs = 'Yes' then 1
-            else 0
-        end as hasTB,
-    datediff(yy, patient.DOB, last_encounter.LastEncounterDate) as AgeLastVisit
-    from distinct_patients
-    left join date_started_TB_treatment on date_started_TB_treatment.PatientPK = distinct_patients.PatientPK
-        and date_started_TB_treatment.SiteCode = distinct_patients.SiteCode
-    left join patient_TB_Diagnosis on patient_TB_Diagnosis.PatientPk = distinct_patients.PatientPK
-        and patient_TB_Diagnosis.SiteCode = distinct_patients.SiteCode
-    left join latest_visit on latest_visit.PatientPK = distinct_patients.PatientPK
-        and latest_visit.SiteCode = distinct_patients.SiteCode
-    left join ODS.dbo.Intermediate_LastPatientEncounter as last_encounter on last_encounter.PatientPK = distinct_patients.PatientPK
-		and last_encounter.SiteCode = distinct_patients.SiteCode
-    left join ODS.dbo.CT_Patient as patient on patient.PatientPK = distinct_patients.PatientPK
-	and patient.SiteCode = distinct_patients.SiteCode
-)
-select 
-    Factkey = IDENTITY(INT, 1, 1),
-    patient.PatientKey,
-	facility.FacilityKey,
-	partner.PartnerKey,
-	agency.AgencyKey,
-    age_group.AgeGroupKey,
-    tb_start_treatment.DateKey as StartTBTreatmentDateKey,
-    tb_diagnosis.DateKey as TBDiagnosisDateKey,
-    combined_ipt_data.OnIPT,
-    combined_ipt_data.hasTB,
-    cast(getdate() as date) as LoadDate
-into NDWH.dbo.FactTPT
-from combined_ipt_data
-left join NDWH.dbo.DimPatient as patient on patient.PatientPKHash = combined_ipt_data.PatientPKHash
-    and patient.SiteCode = combined_ipt_data.SiteCode
-left join NDWH.dbo.DimFacility as facility on facility.MFLCode = combined_ipt_data.SiteCode
-left join NDWH.dbo.DimDate as tb_start_treatment on tb_start_treatment.Date = combined_ipt_data.StartTBTreatmentDate
-left join NDWH.dbo.DimDate as tb_diagnosis on tb_diagnosis.Date = combined_ipt_data.TBDiagnosisDate
-left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = combined_ipt_data.SiteCode
-left join NDWH.dbo.DimPartner as partner on partner.PartnerName = MFL_partner_agency_combination.SDP
-left join NDWH.dbo.DimAgency as agency on agency.AgencyName = MFL_partner_agency_combination.Agency
-left join NDWH.dbo.DimAgeGroup as age_group on age_group.Age = combined_ipt_data.AgeLastVisit
-WHERE patient.voided =0;
+IF OBJECT_ID(N'[NDWH].[dbo].FactTxCurrConcordance', N'U') IS NOT NULL 		
+	drop table [NDWH].[dbo].FactTxCurrConcordance
+GO
 
-alter table NDWH.dbo.FactTPT add primary key(FactKey);
-END
+WITH NDW_CurTx AS (
+                SELECT
+                     SiteCode,
+                   Count(*) AS CurTx_total
+                FROM NDWH.dbo.DimPatient as Patient
+                WHERE isTXcurr =1
+                group by SiteCode
+            
+            ),
+    
+            AllUpload as (
+                SELECT
+                   DateRecieved as DateUploaded,
+                    [SiteCode],
+                    ROW_NUMBER()OVER(Partition by Sitecode Order by DateRecieved Desc) as Num
+                FROM ods.dbo.CT_FacilityManifest m
+                
+            ),
+            Upload As (
+                SELECT distinct
+                    SiteCode,
+                    DateUploaded
+                from AllUpload
+                WHERE Num = 1
+            ),
+            Facilityinfo AS (
+                Select
+                    MFL_Code,
+                    Facility_Name,
+                    County,
+                    SDP as PartnerName,
+                    SDP_Agency as Agency,
+                    EMR
+                from ODS.dbo.All_EMRSites
+            ),
+            EMR As (
+                SELECT
+                    Row_Number () over (partition by FacilityCode order by statusDate desc) as Num,
+                    facilityCode
+                    ,facilityName
+                    ,[value]
+                    ,statusDate
+                    ,indicatorDate
+                FROM ODS.dbo.livesync_Indicator
+                where stage like '%EMR' and name like '%TX_CURR' and indicatorDate= EOMONTH(DATEADD(mm,-1,GETDATE()))
+            ),
+            DHIS2_CurTx AS (
+                SELECT
+                    [SiteCode],
+                    [FacilityName],
+                    [County],
+                    [CurrentOnART_Total],
+                    ReportMonth_Year
+                FROM [ODS].[dbo].[CT_DHIS2]
+                WHERE ReportMonth_Year =CONVERT(VARCHAR(6), DATEADD(MONTH, -1, GETDATE()), 112) and ISNUMERIC(SiteCode) >0
+            ),
+            LatestEMR AS (
+                Select
+                    Emr.facilityCode
+                    ,Emr.facilityName
+                    ,CAST(CONVERT (varchar,Emr.[value] ) AS DECIMAL(10, 4)) As EMRValue
+                    ,Emr.statusDate
+                    ,Emr.indicatorDate
+                from EMR
+                where Num=1
+            ),
+            Uploads as (
+                Select  [DateRecieved],ROW_NUMBER()OVER(Partition by Sitecode Order by [DateRecieved] Desc) as Num ,
+                    SiteCode,
+                    cast( [DateRecieved]as date) As DateReceived,
+                    EmrName as   Emr,
+                    Name,
+                    Start,
+                    PatientCount
+                from ODS.dbo.CT_FacilityManifest
+                where cast  (DateRecieved as date)> DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE())-1, 0) --First day of previous month
+                and cast (DateRecieved as date) <= DATEADD(MONTH, DATEDIFF(MONTH, -1, GETDATE())-1, -1) --Last Day of previous month
+            ),
+            LatestUploads AS (
+                Select
+                    SiteCode,
+                    cast( [DateRecieved]as date) As DateReceived,
+                    Emr,
+                    Name,
+                    Start,
+                    PatientCount
+                from Uploads
+                where Num=1
+            ),
+            Received As (
+                Select distinct
+                Fac.MFL_Code,
+                fac.Facility_Name,
+                Count (*) As Received
+            FROM [ODS].[dbo].[CT_Patient](NoLock) Patient
+            INNER JOIN [ODS].[dbo].[all_EMRSites](NoLock) Fac ON Patient.[SiteCode] = Fac.MFL_Code AND Patient.Voided=0 and Patient.SiteCode>0
+            group by
+                Fac.MFL_Code,
+                fac.Facility_Name
+            ),
+            Facilities AS (
+                Select distinct
+                    MFL_Code ,
+                    Facility_Name,
+                    SDP as PartnerName,
+                    SDP_Agency as AgencyName
+                from ODS.dbo.all_EMRSites
+            ),
+            Combined AS (
+                Select distinct
+                   MFL_Code ,
+                    Facility_Name,
+                    PartnerName,
+                    AgencyName,
+                    LatestUploads.DateReceived,
+                    LatestUploads.PatientCount As ExpectedPatients
+                from Facilities
+                left join LatestUploads on Facilities.MFL_Code =LatestUploads.SiteCode
+            ),
+            Uploaddata AS (
+                Select
+                    Combined.MFL_Code ,
+                    Combined.Facility_Name,
+                    PartnerName,
+                    AgencyName,
+                    DateReceived,
+                    ExpectedPatients,
+                    Received.Received as CompletenessStatus
+                from Combined
+                left join Received on Combined.MFL_Code=Received.MFL_Code
+                where Received<ExpectedPatients
+            ),
+            DWAPI AS (
+                SELECT
+                Sitecode,
+                 DwapiVersion,
+                 Docket
+                        From ODS.dbo.CT_FacilityManifestCargo 
+            ),
+            Summary As (Select
+                coalesce (NDW_CurTx.SiteCode, null ) As MFLCode,
+                fac.Facility_Name As FacilityName,
+                fac.PartnerName,
+                fac.County,
+                fac.emr as EMR,
+                DHIS2_CurTx.CurrentOnART_Total As KHIS_TxCurr,
+                NDW_CurTx.CurTx_total AS DWH_TXCurr,
+                CAST(ROUND(LatestEMR.EMRValue, 2) AS float) AS EMR_TxCurr,
+                CAST(ROUND(LatestEMR.EMRValue - CurTx_total, 2) AS float) AS Diff_EMR_DWH,
+                DHIS2_CurTx.CurrentOnART_Total-CurTx_total As DiffKHISDWH,
+                CAST(ROUND(DHIS2_CurTx.CurrentOnART_Total - LatestEMR.EMRValue, 2) AS FLOAT) AS DiffKHISEMR,
+                CAST(ROUND((CAST(LatestEMR.EMRValue AS DECIMAL(7,2)) - CAST(NDW_CurTx .CurTx_total AS DECIMAL(7,2)))
+                /NULLIF(CAST(LatestEMR.EMRValue  AS DECIMAL(7,2)),0)* 100, 2) AS float) AS Percent_variance_EMR_DWH,
+                CAST(ROUND((CAST(DHIS2_CurTx.CurrentOnART_Total AS DECIMAL(7,2)) - CAST(NDW_CurTx .CurTx_total AS DECIMAL(7,2)))
+                /CAST(DHIS2_CurTx.CurrentOnART_Total  AS DECIMAL(7,2))* 100, 2) AS float) AS Percent_variance_KHIS_DWH,
+                CAST(ROUND((CAST(DHIS2_CurTx.CurrentOnART_Total AS DECIMAL(7,2)) - CAST(LatestEMR.EMRValue AS DECIMAL(7,2)))
+                /CAST(DHIS2_CurTx.CurrentOnART_Total  AS DECIMAL(7,2))* 100, 2) AS float) AS Percent_variance_KHIS_EMR,
+                cast (Upload.DateUploaded as date) As DateUploaded,
+                case when CompletenessStatus is null then 'Complete' else 'Incomplete' End As Completeness,
+                DWAPI.DwapiVersion
+            from NDW_CurTx
+            left join LatestEMR on NDW_CurTx.SiteCode=LatestEMR.facilityCode
+            LEFT JOIN DWAPI ON DWAPI.SiteCode= LatestEMR.facilityCode
+            left join DHIS2_CurTx on NDW_CurTx.SiteCode=DHIS2_CurTx.SiteCode COLLATE Latin1_General_CI_AS
+            left join Upload on NDW_CurTx.SiteCode=Upload.SiteCode
+            left join Uploaddata on NDW_CurTx.SiteCode=Uploaddata.MFL_Code COLLATE Latin1_General_CI_AS
+            left join Facilityinfo fac on NDW_CurTx.SiteCode=fac.MFL_Code
+            
+
+            )
+            Select 
+            FactKey = IDENTITY(INT, 1, 1),
+		    facility.FacilityKey,
+		    partner.PartnerKey,
+		    agency.AgencyKey ,
+            Summary.EMR,
+             KHIS_TxCurr,
+             DWH_TXCurr,
+             EMR_TxCurr,
+            Diff_EMR_DWH,
+            DiffKHISDWH,
+            DiffKHISEMR,
+            Percent_variance_EMR_DWH as Proportion_variance_EMR_DWH,
+            Percent_variance_KHIS_DWH as Proportion_variance_KHIS_DWH ,
+            Percent_variance_KHIS_EMR as Proportion_variance_KHIS_EMR,
+            EOMONTH(DATEADD(mm,-1,GETDATE())) as ReportingMonth,
+            cast (GETDATE() as date) as LoadDate
+            into [NDWH].[dbo].FactTxCurrConcordance
+            from Summary
+left join NDWH.dbo.DimFacility as facility on facility.MFLCode = Summary.MFLCode
+left join Facilityinfo on Facilityinfo.MFL_Code=Summary.MFLCode
+left join NDWH.dbo.DimPartner as partner on partner.PartnerName = Facilityinfo.PartnerName
+left join NDWH.dbo.DimAgency as agency on agency.AgencyName = Facilityinfo.Agency
+alter table NDWH.dbo.FactTxCurrConcordance add primary key(FactKey);
+
+
+
