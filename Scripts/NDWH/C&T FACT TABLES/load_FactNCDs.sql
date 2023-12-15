8bc5ad3be539dffb93ddfb65a6e75c37ed1b1dcd
Mumo Ngungu <nobert.mumo@thepalladiumgroup.com>
2023-12-15 12:40:02 +0300

    change ordering

--- Scripts/NDWH/C&T FACT TABLES/load_FactNCDs.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactNCDs.sql
@@ -91,7 +91,7 @@ hypertensives_ordering as (
         SiteCode,
         VisitDate,
         ChronicIllness,
-        row_number() over (partition by PatientPK, Sitecode order by VisitDate desc) as rank
+        row_number() over (partition by PatientPK, Sitecode order by VisitDate asc) as rank
     from ODS.dbo.CT_AllergiesChronicIllness as chronic
     where chronic.voided = 0 
         and ChronicIllness like '%Hypertension%' 
