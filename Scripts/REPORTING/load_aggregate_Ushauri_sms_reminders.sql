1b42cb1c2fc33cd032511bae0825371d61cf8262
Marymary-dev <marywambu@gmail.com>
2024-01-22 16:32:57 +0300

    Removing redundant CTE, formatting code

--- Scripts/REPORTING/load_aggregate_Ushauri_sms_reminders.sql
+++ Scripts/REPORTING/load_aggregate_Ushauri_sms_reminders.sql
@@ -1,134 +1,100 @@
-IF OBJECT_ID(N'[Reporting].[Dbo].[aggregateUshauriSMSReminders]', N'U') IS NOT NULL DROP TABLE [Reporting].[Dbo].[aggregateUshauriSMSReminders];
+IF Object_id(N'[Reporting].[Dbo].[AggregateUshauriSMSReminders]', N'U') IS NOT NULL
+  DROP TABLE [Reporting].[Dbo].[Aggregateushaurismsreminders];
+
 BEGIN
-   WITH BookedAppointments AS 
-   (
-      SELECT
-         EOMONTH (Try_CONVERT(date, AppointmentDate)) AS AsofDate,
-         COUNT(PatientKey) AS NumberBooked,
-         MFLCode 
-      FROM
-         NDWH.dbo.FactUshauriSMSReminders sms 
-         left join
-            NDWH.dbo.DimFacility fac 
-            on fac.FacilityKey = sms.FacilityKey 
-      WHERE
-         AppointmentStatus is not null 
-      GROUP BY
-         EOMONTH(Try_CONVERT(date, AppointmentDate)),
-         MFLCode 
-   )
-,
-   ConsentedAppointments AS 
-   (
-      SELECT
-         EOMONTH (Try_CONVERT(date, AppointmentDate)) AS AsofDate,
-         COUNT(PatientKey) AS NumberConsented,
-         MFLCode 
-      FROM
-         NDWH.dbo.FactUshauriSMSReminders sms 
-         left join
-            NDWH.dbo.DimFacility fac 
-            on fac.FacilityKey = sms.FacilityKey 
-      WHERE
-         ConsentForSMS = 'YES' 
-      GROUP BY
-         EOMONTH (Try_CONVERT(date, AppointmentDate)),
-         MFLCode 
-   )
-,
-   ReceivedSMS AS 
-   (
-      SELECT
-         COALESCE( Try_CONVERT(date, FourWeekSMSSendDate), Try_CONVERT(date, ThreeWeekSMSSendDate), Try_CONVERT(date, TwoWeekSMSSendDate), Try_CONVERT(date, OneWeekSMSSendDate), Try_CONVERT (date, OneDaySMSSendDate) ) AS AsofDate,
-         COUNT(PatientKey) AS NumberReceivedSMS,
-         MFLCode 
-      FROM
-         NDWH.dbo.FactUshauriSMSReminders sms 
-         left join
-            NDWH.dbo.DimFacility fac 
-            on fac.FacilityKey = sms.FacilityKey 
-      WHERE
-         coalesce (FourWeekSMSSent, ThreeWeekSMSSent, TwoWeekSMSSent, OneWeekSMSSent, OneDaySMSSent) = 'Success' 
-      GROUP BY
-         Try_CONVERT(date, FourWeekSMSSendDate),
-         Try_CONVERT(date, ThreeWeekSMSSendDate),
-         Try_CONVERT(date, TwoWeekSMSSendDate),
-         Try_CONVERT(date, OneWeekSMSSendDate),
-         Try_CONVERT (date, OneDaySMSSendDate),
-         MFLCode 
-   )
-,
-   HonouredAppointments AS 
-   (
-      SELECT
-         EOMONTH(Try_CONVERT(date, AppointmentDate)) AS AsofDate,
-         COUNT(PatientKey) AS NumberHonouredAppointment,
-         MFLCode 
-      FROM
-         NDWH.dbo.FactUshauriSMSReminders sms 
-         left join
-            NDWH.dbo.DimFacility fac 
-            on fac.FacilityKey = sms.FacilityKey 
-      WHERE
-         AppointmentStatus = 'honoured' 
-      GROUP BY
-         EOMONTH (Try_CONVERT(date, AppointmentDate)),
-         MFLCode 
-   )
-,
-   AppointmentCounts AS 
-   (
-      SELECT
-         EOMONTH (Try_CONVERT(date, AppointmentDate)) AS AsofDate,
-         COUNT(PatientKey) AS TotalAppointments,
-         MFLCode 
-      FROM
-         NDWH.dbo.FactUshauriSMSReminders sms 
-         left join
-            NDWH.dbo.DimFacility fac 
-            on fac.FacilityKey = sms.FacilityKey 
-      GROUP BY
-         EOMONTH (Try_CONVERT(date, AppointmentDate)),
-         MFLCode 
-   )
-,
-   Facilityinfo AS 
-   (
-      Select
-         MFL_Code,
-         County,
-         SDP,
-         EMR 
-      from
-         ODS.dbo.All_EMRSites 
-   )
-   SELECT
-      AppointmentCounts.MFLCode,
-      AppointmentCounts.AsofDate,
-      COALESCE(BookedAppointments.NumberBooked, 0) AS NumberBooked,
-      COALESCE(ConsentedAppointments.NumberConsented, 0) AS NumberConsented,
-      COALESCE(ReceivedSMS.NumberReceivedSMS, 0) AS NumberReceivedSMS,
-      COALESCE(HonouredAppointments.NumberHonouredAppointment, 0) AS NumberHonouredAppointment,
-      COALESCE(CAST(HonouredAppointments.NumberHonouredAppointment AS FLOAT) / AppointmentCounts.TotalAppointments * 100, 0) AS PercentHonoured INTO Reporting.Dbo.aggregateUshauriSMSReminders 
-   FROM
-      AppointmentCounts 
-      LEFT JOIN
-         BookedAppointments 
-         ON AppointmentCounts.MFLCode = BookedAppointments.MFLCode 
-         and BookedAppointments.AsofDate = AppointmentCounts.AsofDate 
-      LEFT JOIN
-         ConsentedAppointments 
-         ON AppointmentCounts.MFLCode = ConsentedAppointments.MFLCode 
-         and AppointmentCounts.AsofDate = ConsentedAppointments.AsofDate 
-      LEFT JOIN
-         ReceivedSMS 
-         ON AppointmentCounts.MFLCode = ReceivedSMS.MFLCode 
-         and AppointmentCounts.AsofDate = ReceivedSMS.AsofDate 
-      LEFT JOIN
-         HonouredAppointments 
-         ON AppointmentCounts.MFLCode = ReceivedSMS.MFLCode 
-         and AppointmentCounts.AsofDate = ReceivedSMS.AsofDate 
-   where
-      AppointmentCounts.MFLCode > 0 
-      and AppointmentCounts.AsofDate is not null 
-END
+    WITH Bookedappointments
+         AS (SELECT Asofdate,
+                    Count(Patientkey) AS NumberBooked,
+                    Mflcode
+             FROM   Ndwh.Dbo.Factushaurismsreminders Sms
+                    LEFT JOIN Ndwh.Dbo.Dimfacility Fac
+                           ON Fac.Facilitykey = Sms.Facilitykey
+             WHERE  Appointmentstatus IS NOT NULL
+             GROUP  BY Asofdate,
+                       Mflcode),
+         Consentedappointments
+         AS (SELECT Asofdate,
+                    Count(Patientkey) AS NumberConsented,
+                    Mflcode
+             FROM   Ndwh.Dbo.Factushaurismsreminders Sms
+                    LEFT JOIN Ndwh.Dbo.Dimfacility Fac
+                           ON Fac.Facilitykey = Sms.Facilitykey
+             WHERE  Consentforsms = 'YES'
+             GROUP  BY Asofdate,
+                       Mflcode),
+         Receivedsms
+         AS (SELECT COALESCE(Try_convert(Date, Fourweeksmssenddate),
+                               Try_convert(Date, Threeweeksmssenddate),
+                               Try_convert(Date, Twoweeksmssenddate),
+                               Try_convert(Date, Oneweeksmssenddate),
+                               Try_convert(Date, Onedaysmssenddate)) AS AsofDate
+                    ,
+                    Count(Patientkey)
+                    AS
+                    NumberReceivedSMS
+                    ,
+                    Mflcode
+             FROM   Ndwh.Dbo.Factushaurismsreminders Sms
+                    LEFT JOIN Ndwh.Dbo.Dimfacility Fac
+                           ON Fac.Facilitykey = Sms.Facilitykey
+             WHERE  COALESCE(Fourweeksmssent, Threeweeksmssent, Twoweeksmssent,
+                    Oneweeksmssent,
+                            Onedaysmssent) = 'Success'
+             GROUP  BY Try_convert(Date, Fourweeksmssenddate),
+                       Try_convert(Date, Threeweeksmssenddate),
+                       Try_convert(Date, Twoweeksmssenddate),
+                       Try_convert(Date, Oneweeksmssenddate),
+                       Try_convert(Date, Onedaysmssenddate),
+                       Mflcode),
+         Honouredappointments
+         AS (SELECT Asofdate,
+                    Count(Patientkey) AS NumberHonouredAppointment,
+                    Mflcode
+             FROM   Ndwh.Dbo.Factushaurismsreminders Sms
+                    LEFT JOIN Ndwh.Dbo.Dimfacility Fac
+                           ON Fac.Facilitykey = Sms.Facilitykey
+             WHERE  Appointmentstatus = 'honoured'
+             GROUP  BY Asofdate,
+                       Mflcode),
+         Appointmentcounts
+         AS (SELECT Asofdate,
+                    Count(Patientkey) AS TotalAppointments,
+                    Mflcode
+             FROM   Ndwh.Dbo.Factushaurismsreminders Sms
+                    LEFT JOIN Ndwh.Dbo.Dimfacility Fac
+                           ON Fac.Facilitykey = Sms.Facilitykey
+             GROUP  BY Asofdate,
+                       Mflcode)
+    SELECT Appointmentcounts.Mflcode,
+           Appointmentcounts.Asofdate,
+           COALESCE(Bookedappointments.Numberbooked, 0)                    AS
+           NumberBooked,
+           COALESCE(Consentedappointments.Numberconsented, 0)              AS
+           NumberConsented,
+           COALESCE(Receivedsms.Numberreceivedsms, 0)                      AS
+           NumberReceivedSMS,
+           COALESCE(Honouredappointments.Numberhonouredappointment, 0)     AS
+           NumberHonouredAppointment,
+           COALESCE(Cast(Honouredappointments.Numberhonouredappointment AS Float
+                    ) /
+                             Appointmentcounts.Totalappointments * 100, 0) AS
+           PercentHonoured
+    INTO   Reporting.Dbo.Aggregateushaurismsreminders
+    FROM   Appointmentcounts
+           LEFT JOIN Bookedappointments
+                  ON Appointmentcounts.Mflcode = Bookedappointments.Mflcode
+                     AND Bookedappointments.Asofdate =
+                         Appointmentcounts.Asofdate
+           LEFT JOIN Consentedappointments
+                  ON Appointmentcounts.Mflcode = Consentedappointments.Mflcode
+                     AND Appointmentcounts.Asofdate =
+                         Consentedappointments.Asofdate
+           LEFT JOIN Receivedsms
+                  ON Appointmentcounts.Mflcode = Receivedsms.Mflcode
+                     AND Appointmentcounts.Asofdate = Receivedsms.Asofdate
+           LEFT JOIN Honouredappointments
+                  ON Appointmentcounts.Mflcode = Receivedsms.Mflcode
+                     AND Appointmentcounts.Asofdate = Receivedsms.Asofdate
+    WHERE  Appointmentcounts.Mflcode > 0
+           AND Appointmentcounts.Asofdate IS NOT NULL
+END 
