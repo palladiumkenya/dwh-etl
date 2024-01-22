23519dd7cb6a73a10e1348e8ef502db1f86428e6
Marymary-dev <marywambu@gmail.com>
2024-01-22 22:03:15 +0300

    Tyding up the code

--- Scripts/NDWH/C&T FACT TABLES/load_FactUshauriSMSReminders.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactUshauriSMSReminders.sql
@@ -1,118 +1,110 @@
-IF OBJECT_ID(N'[NDWH].[dbo].[FACTUshauriSMSReminders]', N'U') IS NOT NULL DROP TABLE [NDWH].[dbo].[FACTUshauriSMSReminders];
+IF Object_id(N'[NDWH].[dbo].[FactUshauriSmsReminders]', N'U') IS NOT NULL
+  DROP TABLE [Ndwh].[Dbo].[FactUshauriSmsReminders];
+
 BEGIN
-   With MFL_partner_agency_combination as 
-   (
-      select distinct
-         MFL_Code,
-         SDP,
-         SDP_Agency as Agency 
-      from
-         ODS.dbo.All_EMRSites 
-   )
-   Select
-      FactKey = IDENTITY(INT, 1, 1),
-      facility.FacilityKey,
-      partner.PartnerKey,
-      patient.PatientKey,
-      agency.AgencyKey,
-      EOMONTH(Try_CONVERT(date, AppointmentDate)) AS AsofDate,
-      apt.MaritalStatus,
-      age_group.AgeGroupKey,
-      cast (appointment.Datekey as date) as AppointmentDate,
-      AppointmentType,
-      AppointmentStatus,
-      EntryPoint,
-      VisitType,
-      cast(attended.DateKey as date) as DateAttended,
-      apt.DOB,
-      ConsentForSMS,
-      SMSLanguage,
-      SMSTargetGroup,
-      SMSPreferredSendTime,
-      FourWeekSMSSent,
-      cast (FourweeksDate.Datekey as date) as FourWeekSMSSendDate,
-      FourWeekSMSDeliveryStatus,
-      FourWeekSMSDeliveryFailureReason,
-      ThreeWeekSMSSent,
-      Cast (ThreeweeksDate.Datekey as date) as ThreeWeekSMSSendDate,
-      ThreeWeekSMSDeliveryStatus,
-      ThreeWeekSMSDeliveryFailureReason,
-      TwoWeekSMSSent,
-      Cast (TwoweeksDate.Datekey as date) as TwoWeekSMSSendDate,
-      TwoWeekSMSDeliveryStatus,
-      TwoWeekSMSDeliveryFailureReason,
-      OneWeekSMSSent,
-      Cast (OneweeksDate.Datekey as date) as OneWeekSMSSendDate,
-      OneWeekSMSDeliveryStatus,
-      OneWeekSMSDeliveryFailureReason,
-      OneDaySMSSent,
-      Cast (OneDayDate.Datekey as date) as OneDaySMSSendDate,
-      OneDaySMSDeliveryStatus,
-      OneDaySMSDeliveryFailureReason,
-      MissedAppointmentSMSSent,
-      Cast (MissedappointmentDate.Datekey as date) as MissedAppointmentSMSSendDate,
-      MissedAppointmentSMSDeliveryStatus,
-      MissedAppointmentSMSDeliveryFailureReason,
-      TracingCalls,
-      TracingSMS,
-      TracingHomeVisits,
-      TracingOutcome,
-      Cast (TracingDate.Datekey as date) As TracingOutcomeDate,
-      Cast (DateReturnedToCare.DateKey as date ) as DateReturnedToCare,
-      DaysDefaulted,
-      NUPIHash into NDWH.dbo.FACTUshauriSMSReminders 
-   FROM
-      ODS.dbo.Ushauri_PatientAppointments as apt 
-      left join
-         NDWH.dbo.DimFacility as facility 
-         on facility.MFLCode = apt.sitecode 
-      left join
-         MFL_partner_agency_combination 
-         on MFL_partner_agency_combination.MFL_Code = apt.sitecode 
-      left join
-         NDWH.dbo.DimPartner as partner 
-         on partner.PartnerName = MFL_partner_agency_combination.SDP 
-      left join
-         NDWH.dbo.DimPatient as patient 
-         on patient.PatientPKHash = apt.PatientPKhash 
-         and patient.SiteCode = apt.sitecode 
-      left join
-         NDWH.dbo.DimAgency as agency 
-         on agency.AgencyName = MFL_partner_agency_combination.Agency 
-      left join
-         NDWH.dbo.DimAgeGroup as age_group 
-         on age_group.AgeGroupKey = DATEDIFF(YY, patient.DOB, Try_Convert (date, AppointmentDate)) 
-      left join
-         NDWH.dbo.DimDate as as_of 
-         on as_of.Date = Try_Convert(date, apt.AppointmentDate) 
-      left join
-         NDWH.dbo.DimDate as appointment 
-         on appointment.Date = Try_Convert(date, apt.AppointmentDate) 
-      left join
-         NDWH.dbo.DimDate as attended 
-         on attended.Date = Try_Convert(date, apt.DateAttended) 
-      left join
-         NDWH.dbo.DimDate as FourweeksDate 
-         on FourweeksDate.Date = Try_Convert(date, apt.FourWeekSMSSendDate) 
-      left join
-         NDWH.dbo.DimDate as ThreeweeksDate 
-         on ThreeweeksDate.Date = Try_convert(date, apt.ThreeWeekSMSSendDate) 
-      left join
-         NDWH.dbo.DimDate as TwoweeksDate 
-         on TwoweeksDate.Date = Try_Convert(date, apt.TwoWeekSMSSendDate) 
-      left join
-         NDWH.dbo.DimDate as OneweeksDate 
-         on OneweeksDate.Date = Try_Convert(date, apt.OneWeekSMSSendDate) 
-      left join
-         NDWH.dbo.DimDate as OneDayDate 
-         on OneDayDate.Date = Try_Convert(date, apt.OneDaySMSSendDate) 
-      left join
-         NDWH.dbo.DimDate as MissedAppointmentDate 
-         on MissedAppointmentDate.Date = Try_Convert(date, apt.MissedAppointmentSMSSendDate) 
-      left join
-         NDWH.dbo.DimDate as TracingDate 
-         on TracingDate.Date = Try_Convert(date, apt.TracingOutcomeDate) 
-      left join
-         NDWH.dbo.DimDate as DateReturnedToCare 
-         on DateReturnedToCare.Date = Try_Convert(date, apt.DateReturnedToCare) alter table NDWH.dbo.FACTUshauriSMSReminders add primary key(FactKey);
-END
+    WITH Mfl_partner_agency_combination
+         AS (SELECT DISTINCT Mfl_code,
+                             Sdp,
+                             Sdp_agency AS Agency
+             FROM   Ods.Dbo.All_emrsites)
+
+    SELECT FactKey = IDENTITY(Int, 1, 1),
+           Facility.Facilitykey,
+           Partner.Partnerkey,
+           Patient.Patientkey,
+           Agency.Agencykey,
+           EOMONTH(TRY_CONVERT(date, Appointmentdate)) AS AsofDate, ---This date has conversion issues----
+           Apt.Maritalstatus,
+           Age_group.Agegroupkey,
+           Appointment.Datekey           AS AppointmentDateKey,
+           Appointmenttype,
+           Appointmentstatus,
+           Entrypoint,
+           Visittype,
+           Attended.Datekey               AS DateAttendedDateKey,
+           Consentforsms,
+           Smslanguage,
+           Smstargetgroup,
+           Smspreferredsendtime,
+           Fourweeksmssent,
+           Fourweeksdate.Datekey         AS FourWeekSMSSendDateKey,
+           Fourweeksmsdeliverystatus,
+           Fourweeksmsdeliveryfailurereason,
+           Threeweeksmssent,
+           Threeweeksdate.Datekey        AS ThreeWeekSMSSendDateKey,
+           Threeweeksmsdeliverystatus,
+           Threeweeksmsdeliveryfailurereason,
+           Twoweeksmssent,
+           Twoweeksdate.Datekey          AS TwoWeekSMSSendDateKey,
+           Twoweeksmsdeliverystatus,
+           Twoweeksmsdeliveryfailurereason,
+           Oneweeksmssent,
+           Oneweeksdate.Datekey          AS OneWeekSMSSendDateKey,
+           Oneweeksmsdeliverystatus,
+           Oneweeksmsdeliveryfailurereason,
+           Onedaysmssent,
+           Onedaydate.Datekey           AS OneDaySMSSendDateKey,
+           Onedaysmsdeliverystatus,
+           Onedaysmsdeliveryfailurereason,
+           Missedappointmentsmssent,
+           Missedappointmentdate.Datekey  AS  MissedAppointmentSMSSendDateKey,
+           Missedappointmentsmsdeliverystatus,
+           Missedappointmentsmsdeliveryfailurereason,
+           Tracingcalls,
+           Tracingsms,
+           Tracinghomevisits,
+           Tracingoutcome,
+           Tracingdate.Datekey          AS TracingOutcomeDateKey,
+           Datereturnedtocare.Datekey    AS DateReturnedToCareDateKey,
+           Daysdefaulted,
+           Nupihash
+    INTO   Ndwh.Dbo.FactUshauriSmsReminders
+    FROM   Ods.Dbo.Ushauri_patientappointments AS Apt
+           LEFT JOIN Ndwh.Dbo.Dimfacility AS Facility
+                  ON Facility.Mflcode = Apt.Sitecode
+           LEFT JOIN Mfl_partner_agency_combination
+                  ON Mfl_partner_agency_combination.Mfl_code = Apt.Sitecode
+           LEFT JOIN Ndwh.Dbo.Dimpartner AS Partner
+                  ON Partner.Partnername = Mfl_partner_agency_combination.Sdp
+           LEFT JOIN Ndwh.Dbo.Dimpatient AS Patient
+                  ON Patient.Patientpkhash = Apt.Patientpkhash
+                     AND Patient.Sitecode = Apt.Sitecode
+           LEFT JOIN Ndwh.Dbo.Dimagency AS Agency
+                  ON Agency.Agencyname = Mfl_partner_agency_combination.Agency
+           LEFT JOIN Ndwh.Dbo.Dimagegroup AS Age_group
+                  ON Age_group.Agegroupkey = Datediff(Yy, Patient.Dob,
+                                             Try_convert (Date, Appointmentdate))
+           LEFT JOIN Ndwh.Dbo.Dimdate AS As_of
+                  ON As_of.Date = Try_convert(Date, Apt.Appointmentdate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Appointment
+                  ON Appointment.Date = Try_convert(Date, Apt.Appointmentdate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Attended
+                  ON Attended.Date = Try_convert(Date, Apt.Dateattended)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Fourweeksdate
+                  ON Fourweeksdate.Date =
+                     Try_convert(Date, Apt.Fourweeksmssenddate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Threeweeksdate
+                  ON Threeweeksdate.Date =
+                     Try_convert(Date, Apt.Threeweeksmssenddate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Twoweeksdate
+                  ON Twoweeksdate.Date =
+                     Try_convert(Date, Apt.Twoweeksmssenddate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Oneweeksdate
+                  ON Oneweeksdate.Date =
+                     Try_convert(Date, Apt.Oneweeksmssenddate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Onedaydate
+                  ON Onedaydate.Date = Try_convert(Date, Apt.Onedaysmssenddate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Missedappointmentdate
+                  ON Missedappointmentdate.Date =
+                     Try_convert(Date, Apt.Missedappointmentsmssenddate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Tracingdate
+                  ON Tracingdate.Date =
+                     Try_convert(Date, Apt.Tracingoutcomedate)
+           LEFT JOIN Ndwh.Dbo.Dimdate AS Datereturnedtocare
+                  ON Datereturnedtocare.Date =
+                     Try_convert(Date, Apt.Datereturnedtocare)
+
+    ALTER TABLE Ndwh.Dbo.Factushaurismsreminders
+      ADD PRIMARY KEY(Factkey);
+END 
+
