4724e86435506bc762b4b1469fee0bb6fbc691cc
Mugo <dennis.ndwiga@thepalladiumgroup.com>
2024-01-31 17:24:49 +0300

    Modified CT_patient

--- Scripts/ODS/CT_DOCKET/Load_CT_Patient.sql
+++ Scripts/ODS/CT_DOCKET/Load_CT_Patient.sql
@@ -42,6 +42,7 @@ BEGIN
 						WHEN MATCHED THEN
 							UPDATE SET 				
 							a.PatientID					=b.PatientID,
+							a.nupi       				 = b.nupi,
 							a.PatientSource				=b.PatientSource,							
 							a.ContactRelation			=b.ContactRelation,
 							a.LastVisit					=b.LastVisit,
