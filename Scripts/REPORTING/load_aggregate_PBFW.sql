c262c1a2ef4fc9150f3cbb43bfead0ab813e2c4b
Marymary-dev <marywambu@gmail.com>
2024-02-12 23:10:59 +0300

    Fixing the unsuppressed numbers

--- Scripts/REPORTING/load_aggregate_PBFW.sql
+++ Scripts/REPORTING/load_aggregate_PBFW.sql
@@ -16,10 +16,10 @@ SELECT
     SUM (case when Recieivedart=1 Then 1 Else 0 End ) As  PBFWOnART,
     SUM (Case When  Recieivedart=1  and EligibleVL=1 Then  1 Else 0 end) AS PBFWEligiblevl,
     SUM (CASE WHEN PBFW_ValidVLResultCategory  is not null Then 1   ELSE 0 END) AS PBFWValidVl,
-    SUM (Case When HasValidVL=1 and PBFW_ValidVLSup=1 Then 1 else 0 End) AS PBFWSuppressed,
-    SUM (Case When HasValidVL=1 and PBFW_ValidVLSup=0 Then 1 Else 0 End ) AS PBFWUnsuppressed,
+    SUM (Case When PBFW_ValidVLResultCategory is not null and PBFW_ValidVLSup=1 Then 1 else 0 End) AS PBFWSuppressed,
+    SUM(CASE  WHEN PBFW_ValidVLResultCategory is not null  AND ISNUMERIC(PBFW_ValidVLSup) = 1  AND CAST(PBFW_ValidVLSup AS INT) = 0   THEN 1   ELSE 0 END) AS PBFWUnsuppressed,
     Sum (Case When RepeatVls=1 Then 1 Else 0 End) As PBFWRepeatVl,
-     Sum (Case When RepeatSuppressed=1 Then 1 Else 0 End) As PBFWRepeatVlSuppressed,
+    Sum (Case When RepeatSuppressed=1 Then 1 Else 0 End) As PBFWRepeatVlSuppressed,
     Sum (Case When RepeatUnsuppressed=1 Then 1 Else 0 End) As PBFWRepeatVlUnSuppressed,
     Sum (Case When RepeatUnsuppressed=1 and ReceivedEAC1=1 Then 1 else 0 End) As PBFWUnsupReceivedEAC1,
     Sum (Case When RepeatUnsuppressed=1 and ReceivedEAC2=1 Then 1 Else 0 End) As PBFWUnsupReceivedEAC2,
@@ -43,4 +43,5 @@ GROUP BY
     Agency.AgencyName,
     Age_group.DATIMAgeGroup,
     Patient.Gender;
+
 
