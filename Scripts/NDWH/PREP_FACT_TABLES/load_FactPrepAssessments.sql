b3c7fc6bd1088fab2be7315de02c96fdb989e78f
Marymary-dev <marywambu@gmail.com>
2024-04-03 12:31:21 +0300

    Selecting the PrepEnrollmentDate and assessmentDate

--- Scripts/NDWH/PREP_FACT_TABLES/load_FactPrepAssessments.sql
+++ Scripts/NDWH/PREP_FACT_TABLES/load_FactPrepAssessments.sql
@@ -36,7 +36,8 @@ source_data as (
             when ClientRisk='Risk' then 1 else 0 end as EligiblePrep,
         VisitDate As AssessmentVisitDate,
         case 
-            when VisitDate is not null then 1 else 0 end as ScreenedPrep
+            when VisitDate is not null then 1 else 0 end as ScreenedPrep,
+            PrepEnrollmentDate
     from ODS.dbo.PrEP_Patient as patient
     left join ODS.dbo.PrEP_BehaviourRisk as risk on patient.PatientPk = risk.PatientPk
         and patient.SiteCode = risk.SiteCode
@@ -68,8 +69,8 @@ select
     SexWithoutCondom,
     NumberofchildrenWithPartner,
     ClientRisk,
-    assessment_date.DateKey As AssessmentVisitDateKey,
-    patient.PrepEnrollmentDateKey,
+    AssessmentVisitDate,
+    PrepEnrollmentDate,
     EligiblePrep,
     ScreenedPrep,
     cast(getdate() as date) as LoadDate
@@ -83,6 +84,7 @@ left join NDWH.dbo.DimAgency as agency on agency.AgencyName = MFL_partner_agency
 left join NDWH.dbo.DimFacility as facility on facility.MFLCode = source_data.SiteCode
 left join NDWH.dbo.DimAgeGroup as age_group on age_group.Age = datediff(yy, patient.DOB, coalesce(source_data.AssessmentVisitDate, getdate()))
 left join NDWH.dbo.DimDate as assessment_date on assessment_date.Date = source_data.AssessmentVisitDate
+left join NDWH.dbo.DimDate as prep_Date on prep_Date.Date = source_data.PrepEnrollmentDate
 
 
 WHERE patient.voided =0;
