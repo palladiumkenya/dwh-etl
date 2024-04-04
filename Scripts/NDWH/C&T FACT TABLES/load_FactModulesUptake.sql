4467e34a69b068702177f3ee76f10ce6bba57732
Marymary-dev <marywambu@gmail.com>
2024-04-04 17:34:53 +0300

    Refactor to have the each facility per row

--- Scripts/NDWH/C&T FACT TABLES/load_FactModulesUptake.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactModulesUptake.sql
@@ -3,67 +3,67 @@ If Object_id(N'[NDWH].[dbo].[FactModulesuptake]', N'U') Is Not Null
 
 Begin
     With Sites
-         As (Select Sdp,
+         As (Select 
+         distinct  Mfl_code ,
+                   Sdp,
                     Sdp_agency,
                     County,
-                    Count (Mfl_code) EMRSites
+                    Subcounty,
+                    case when Mfl_code is not null then 1 Else 0 End as isEMRSite
              From   Ods.Dbo.All_emrsites
              Where  Emr_status = 'Active'
-             Group  By Sdp,
-                       Sdp_agency,
-                       County),
+             ),
          Otz
-         As (Select Sdp,
-                    Sdp_agency,
-                    County,
-                    Count (Distinct Sitecode) OTZ
+         As (Select 
+                   Distinct Sitecode,
+                  case when Sitecode is not null then 1 Else 0 End as isOTZ,
+                  Sdp,
+                  Sdp_agency,
+                  County
              From   Ods.Dbo.Ct_otz Otz
                     Left Join Ods.Dbo.All_emrsites Emr
                            On Emr.Mfl_code = Otz.Sitecode
              Where  Datediff(Month, Visitdate, Eomonth(Dateadd(Mm, -1, Getdate())))<= 12
-             Group  By Sdp,
-                       Sdp_agency,
-                       County),
+            ),
          Ovc
-         As (Select Sdp,
-                    Sdp_agency,
-                    County,
-                    Count (Distinct Sitecode) OVC
+         As (Select 
+                  Distinct Sitecode,
+                  case when Sitecode is not null then 1 Else 0 End as isOVC,
+                  Sdp,
+                  Sdp_agency,
+                  County
              From   Ods.Dbo.Ct_ovc Ovc
                     Left Join Ods.Dbo.All_emrsites Emr
                            On Emr.Mfl_code = Ovc.Sitecode
              Where  Datediff(Month, Visitdate, Eomonth( Dateadd(Mm, -1, Getdate()))) <= 12
-             Group  By Sdp,
-                       Sdp_agency,
-                       County),
+             ),
          Hts
-         As (Select Sdp,
-                    Sdp_agency,
-                    County,
-                    Count (Distinct Sitecode) HTS
+         As (Select 
+                  Distinct Sitecode,
+                  case when Sitecode is not null then 1 Else 0 End as isHTS,
+                  Sdp,
+                  Sdp_agency,
+                  County
              From   Ods.Dbo.Hts_clienttests Tests
                     Left Join Ods.Dbo.All_emrsites Emr
                            On Emr.Mfl_code = Tests.Sitecode
              Where  Datediff(Month, Testdate, Eomonth(Dateadd(Mm, -1, Getdate())
-                                              ))
-                    <= 12
-             Group  By Sdp,
-                       Sdp_agency,
-                       County),
+                                              )) <= 12
+            ),
          Prep
-         As (Select Sdp,
-                    Sdp_agency,
-                    County,
-                    Count (Distinct Prep.Sitecode) Prep
+         As (Select 
+                  Distinct prep.Sitecode,
+                  case when prep.SiteCode is not null then 1 Else 0 End as isPrep,
+                  Sdp,
+                  Sdp_agency,
+                  County
              From   Ods.Dbo.Prep_visits Prep
                     Left Join Ods.Dbo.All_emrsites Emr
                            On Emr.Mfl_code = Prep.Sitecode
                     Full Join Ods.Dbo.Prep_behaviourrisk Beha
                            On Beha.Sitecode = Prep.Sitecode
              Where  Datediff(Month, Prep.Visitdate, Eomonth( Dateadd(Mm, -1, Getdate()))) <= 12
-             Group  By Sdp,
-                       Sdp_agency,
-                       County),
+             ),
          Combined_dataset
          As (Select Patientpkhash,
                     Sitecode,
@@ -80,96 +80,95 @@ Begin
                     Visitdate
              From   Ods.Dbo.Mnch_pncvisits),
          Pmtct
-         As (Select Sdp,
-                    Sdp_agency,
-                    County,
-                    Count(Distinct Sitecode) As PMTCT
+         As (Select 
+                 Distinct Sitecode,
+                  case when Sitecode is not null then 1 Else 0 End as isPMTCT,
+                  Sdp,
+                  Sdp_agency,
+                  County
              From   Combined_dataset
                     Left Join Ods.Dbo.All_emrsites As Sites
                            On Sites.Mfl_code = Combined_dataset.Sitecode
              Where  Datediff(Month, Visitdate, Eomonth(Dateadd(Mm, -1, Getdate())))<= 12
-             Group  By Sdp,
-                       Sdp_agency,
-                       County),
+             ),
          Iit
-         As (Select Sdp,
-                    Sdp_agency,
-                    County,
-                    Count (Distinct Sitecode) IIT
+         As (Select 
+                  Distinct Sitecode,
+                  case when Sitecode is not null then 1 Else 0 End as isIITML,
+                  Sdp,
+                  Sdp_agency,
+                  County
              From   Ods.Dbo.Ct_iitriskscores Iit
                     Left Join Ods.Dbo.All_emrsites Emr
                            On Emr.Mfl_code = Iit.Sitecode
              Where  Datediff(Month, Riskevaluationdate, Eomonth(Dateadd(Mm, -1, Getdate()))) <= 12
-             Group  By Sdp,
-                       Sdp_agency,
-                       County),
+             ),
          Htsml
-         As (Select Sdp,
-                    Sdp_agency,
-                    County,
-                    Count (Distinct Sitecode) HTSML
+         As (Select 
+                  Distinct Sitecode,
+                  case when Sitecode is not null then 1 Else 0 End as isHTSML,
+                  Sdp,
+                  Sdp_agency,
+                  County
              From   Ods.Dbo.Hts_eligibilityextract Htsml
                     Left Join Ods.Dbo.All_emrsites Emr
                            On Emr.Mfl_code = Htsml.Sitecode
              Where  Datediff(Month, Visitdate, Eomonth( Dateadd(Mm, -1, Getdate())))  <= 12 and HIVRiskCategory is not null
-             Group  By Sdp,
-                       Sdp_agency,
-                       County),
+             ),
          Summary
-         As (Select Sites.Sdp,
-                    Sites.Sdp_agency,
-                    Sites.County,
-                    Emrsites,
-                    Coalesce (Otz, 0)   As OTZ,
-                    Coalesce (Ovc, 0)   As OVC,
-                    Coalesce (Hts, 0)   As HTS,
-                    Coalesce (Prep, 0)  As Prep,
-                    Coalesce (Pmtct, 0) As PMTCT,
-                    Coalesce (Iit, 0)   As IIT,
-                    Coalesce (Htsml, 0) As HTSML
+         As (Select 
+                    Mfl_code ,
+                    sites.Sdp,
+                    sites.SDP_Agency,
+                    sites.County,
+                    Subcounty,
+                    coalesce (isEMRSite,0) as isEMRSite,
+                    coalesce (isOTZ,0) as isOTZ,
+                    coalesce (isOVC,0) as isOVC,
+                    coalesce (isHTS,0) as isHTS,
+                    coalesce (isPrep,0) as isPrep,
+                    coalesce (isPMTCT,0) as isPMTCT,
+                    coalesce (isIITML,0) as isIITML,
+                    coalesce (isHTSML,0) as isHTSML
              From   Sites
                     Left Join Otz
-                           On Otz.Sdp = Sites.Sdp
-                              And Otz.Sdp_agency = Sites.Sdp_agency
-                              And Otz.County=Sites.County
+                           On Otz.SiteCode = Sites.MFL_Code
+                           and Otz.SDP=Sites.SDP
                     Left Join Ovc
-                           On Ovc.Sdp = Sites.Sdp
-                              And Ovc.Sdp_agency = Sites.Sdp_agency
-                              And Ovc.County=Sites.County
+                           On Ovc.SiteCode = Sites.MFL_Code  
+                            and Ovc.SDP=Sites.SDP  
                     Left Join Hts
-                           On Hts.Sdp = Sites.Sdp
-                              And Hts.Sdp_agency = Sites.Sdp_agency
-                              And Hts.County=Sites.County
+                           On Hts.SiteCode = Sites.MFL_Code
+                            and Hts.SDP=Sites.SDP
                     Left Join Prep
-                           On Prep.Sdp = Sites.Sdp
-                              And Prep.Sdp_agency = Sites.Sdp_agency
-                              And Prep.County=Sites.County
+                           On Prep.SiteCode = Sites.MFL_Code
+                            and Prep.SDP=Sites.SDP
                     Left Join Pmtct
-                           On Pmtct.Sdp = Sites.Sdp
-                              And Pmtct.Sdp_agency = Sites.Sdp_agency
-                              And Pmtct.County=Sites.County
+                           On Pmtct.SiteCode = Sites.MFL_Code
+                            and Pmtct.SDP=Sites.SDP
                     Left Join Iit
-                           On Iit.Sdp = Sites.Sdp
-                              And Iit.Sdp_agency = Sites.Sdp_agency
-                              And Iit.County=Sites.County
+                           On Iit.SiteCode = Sites.MFL_Code
+                            and Iit.SDP=Sites.SDP
                     Left Join Htsml
-                           On Htsml.Sdp = Sites.Sdp
-                              And Htsml.Sdp_agency = Sites.Sdp_agency
-                              And Htsml.County=Sites.County)
-    Select Factkey = Identity(Int, 1, 1),
+                           On Htsml.SiteCode = Sites.MFL_Code
+                            and Htsml.SDP=Sites.SDP
+                              )
+    Select 
+           Factkey = Identity(Int, 1, 1),
            Partner.Partnerkey,
            Agency.Agencykey,
-           EMRSites,
-           County,
-           Otz,
-           Ovc,
-           Hts,
-           Prep,
-           Pmtct,
-           Iit,
-           Htsml
+           fac.FacilityKey,
+           isEMRSite,
+           Summary.isHTS,
+           isHTSML,
+           isIITML,
+           isOTZ,
+           isOVC,
+           isPMTCT,
+           isPrep
     Into   Ndwh.Dbo.FactModulesuptake
     From   Summary
+           Left join NDWH.dbo.DimFacility as fac on fac.MFLCode=Summary.MFL_Code
            Left Join Ndwh.Dbo.Dimpartner As Partner
                   On Partner.Partnername = Summary.Sdp
            Left Join Ndwh.Dbo.Dimagency As Agency
