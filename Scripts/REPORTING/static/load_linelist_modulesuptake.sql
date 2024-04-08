373ea939919f5addf7a49c4603d06aec68fcaf11
Marymary-dev <marywambu@gmail.com>
2024-04-08 12:53:08 +0300

    Adding linelist modules uptake

---
+++ Scripts/REPORTING/static/load_linelist_modulesuptake.sql
@@ -0,0 +1,31 @@
+IF OBJECT_ID(N'[REPORTING].[dbo].LinelistModuleUptake', N'U') IS NOT NULL 
+    DROP TABLE [REPORTING].[dbo].LinelistModuleUptake;
+
+
+WITH ModulesUptake AS (
+    SELECT
+        MFLCode,
+        FacilityName,
+        SubCounty,
+        County,
+        isEMRSite,
+        PartnerName,
+        AgencyName,
+        modules.isHTS,
+        isHTSML,
+        isIITML,
+        isOTZ,
+        isPMTCT,
+        isPrep,
+        CAST(GETDATE() AS DATE) AS LoadDate 
+    FROM NDWH.dbo.FactModulesuptake AS modules
+    LEFT JOIN NDWH.dbo.DimFacility fac ON fac.FacilityKey = modules.FacilityKey
+    LEFT JOIN NDWH.dbo.DimPartner pat ON pat.PartnerKey = modules.Partnerkey
+    LEFT JOIN NDWH.dbo.DimAgency agency ON agency.AgencyKey = modules.Agencykey
+)
+
+
+SELECT *
+INTO REPORTING.dbo.LinelistModuleUptake 
+FROM ModulesUptake;
+
