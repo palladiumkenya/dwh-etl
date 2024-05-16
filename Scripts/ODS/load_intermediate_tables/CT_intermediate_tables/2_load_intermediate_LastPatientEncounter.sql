d5069271a12d7098948188c18e732d9ee39ae43a
Marymary-dev <marywambu@gmail.com>
2024-05-16 12:08:50 +0300

    Refactor last Encounter model to pick the max Expected return from the pharmacy table due to existing duplicates

--- Scripts/ODS/load_intermediate_tables/CT_intermediate_tables/2_load_intermediate_LastPatientEncounter.sql
+++ Scripts/ODS/load_intermediate_tables/CT_intermediate_tables/2_load_intermediate_LastPatientEncounter.sql
@@ -3,14 +3,49 @@ IF OBJECT_ID(N'[ODS].[dbo].[Intermediate_LastPatientEncounter]', N'U') IS NOT NU
 BEGIN
 
 --Pick the latest LastVisit and Next Appointment dates from Pharmacy
-    WITH Pharmacy AS (
-    SELECT   ROW_NUMBER()OVER (PARTITION by SiteCode,PatientPK  ORDER BY DispenseDate Desc ) As NUM ,
-         SiteCode,
-         PatientPK ,
-         DispenseDate As LastEncounterDate,
-        Case When DATEDIFF(dd,DispenseDate,ExpectedReturn) >= 365 or ExpectedReturn ='1900-01-01' or  ExpectedReturn is null  THEN DATEADD(dd,30,DispenseDate) ELSE ExpectedReturn End as NextAppointmentDate
-     FROM ODS.dbo.CT_PatientPharmacy  As LastEncounter
-     where DispenseDate <= EOMONTH(DATEADD(mm,-1,GETDATE())) and LastEncounter.VOIDED=0
+
+WITH PharmacyRecords AS (
+    SELECT
+        ROW_NUMBER() OVER (PARTITION BY SiteCode, PatientPK, DispenseDate ORDER BY DispenseDate DESC) AS NUM,
+        SiteCode,
+        PatientPK,
+        DispenseDate AS LastEncounterDate,
+        CASE
+            WHEN DATEDIFF(dd, DispenseDate, ExpectedReturn) >= 365 OR ExpectedReturn = '1900-01-01' OR ExpectedReturn IS NULL THEN DATEADD(dd, 30, DispenseDate)
+            ELSE MAX(ExpectedReturn)
+        END AS NextAppointmentDate
+    FROM
+        ODS.dbo.CT_PatientPharmacy AS LastEncounter
+    WHERE
+
+     DispenseDate <= EOMONTH(DATEADD(mm, -1, GETDATE()))
+        AND LastEncounter.VOIDED = 0
+    GROUP BY
+        SiteCode,
+        PatientPK,
+        DispenseDate,
+        ExpectedReturn
+),
+Pharmacy as (SELECT
+    SiteCode,
+    PatientPK,
+    LastEncounterDate,
+    NextAppointmentDate
+FROM
+    (
+        SELECT
+            SiteCode,
+            PatientPK,
+            LastEncounterDate,
+            NextAppointmentDate,
+            ROW_NUMBER() OVER (PARTITION BY SiteCode, PatientPK ORDER BY NextAppointmentDate DESC) AS RowNumber
+        FROM
+            PharmacyRecords
+    ) AS Sub
+WHERE
+    RowNumber = 1
+
+
 ),
 --Pick Expected return and Lastvisit  dates from ARTPatient only if Expected return is <365days and add 30 days to Last visit if it is null
 ART_expected_dates_logic AS (
@@ -55,7 +90,7 @@ PharmacyART_Visits As (
     Then Pharmacy.LastEncounterDate Else ART_expected_dates_logic.Lastvisit End As LastVisitART_Pharmacy,
      Case when Pharmacy.NextAppointmentdate>=ART_expected_dates_logic.expectedReturn or ART_expected_dates_logic.expectedReturn is null  Then Pharmacy.NextAppointmentdate else ART_expected_dates_logic.expectedReturn End as NextappointmentDate
     from Patients
-    left join Pharmacy on  Patients.PatientPk=Pharmacy.PatientPk and Patients.Sitecode=Pharmacy.Sitecode and Num=1
+    left join Pharmacy on  Patients.PatientPk=Pharmacy.PatientPk and Patients.Sitecode=Pharmacy.Sitecode 
     left join ART_expected_dates_logic on Patients.PatientPk=ART_expected_dates_logic.PatientPk and Patients.Sitecode=ART_expected_dates_logic.Sitecode
 ),
 --compare the results of the Pharmacy and ART above with when date add has been applied for the patients mising  TCAs and pick the greater
@@ -102,3 +137,5 @@ from CombinedVisits
 where LastEncounterDate <= EOMONTH(DATEADD(mm,-1,GETDATE()))
 
 END
+
+
