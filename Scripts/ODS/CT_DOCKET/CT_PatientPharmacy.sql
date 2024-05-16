766007c31927467ada32fb866b0bcf19d20d5cfe
Mugo <dennis.ndwiga@thepalladiumgroup.com>
2024-05-16 11:11:02 +0300

    Removed the visitID in the uniqueness check under the ct_patientPharmacy and also removed the ID check during the merging of records. For the other extracts, Iimproved the readability of the scripts

--- Scripts/ODS/CT_DOCKET/CT_PatientPharmacy.sql
+++ Scripts/ODS/CT_DOCKET/CT_PatientPharmacy.sql
@@ -1,28 +1,6 @@
+
 BEGIN
-
-					;with cte AS ( Select            
-					P.PatientPID,            
-					PP.PatientId,            
-					F.code,
-					PP.VisitID,
-					PP.DispenseDate,
-					PP.created,  ROW_NUMBER() OVER (PARTITION BY P.PatientPID,F.code ,PP.VisitID,PP.DispenseDate
-					ORDER BY PP.created desc) Row_Num
-			FROM [DWAPICentral].[dbo].[PatientExtract] P 
-						INNER JOIN [DWAPICentral].[dbo].[PatientPharmacyExtract] PP ON PP.[PatientId]= P.ID AND PP.Voided=0
-						INNER JOIN [DWAPICentral].[dbo].[Facility] F ON P.[FacilityId] = F.Id AND F.Voided=0
-					WHERE p.gender!='Unknown'  )      
-		
-			delete pb from  [DWAPICentral].[dbo].[PatientPharmacyExtract](NoLock) pb
-			inner join [DWAPICentral].[dbo].[PatientExtract](NoLock) P ON PB.[PatientId]= P.ID AND PB.Voided = 0       
-			inner join [DWAPICentral].[dbo].[Facility](NoLock) F ON P.[FacilityId] = F.Id AND F.Voided=0       
-			inner join cte on pb.PatientId = cte.PatientId  
-				and cte.Created = pb.created 
-				and cte.Code =  f.Code     
-				and cte.VisitID = pb.VisitID
-				and cte.DispenseDate = pb.DispenseDate
-			where  Row_Num  > 1;
-
+				
 			DECLARE @MaxDispenseDate_Hist			DATETIME,
 				  @DispenseDate					DATETIME,
 				  @MaxCreatedDate				DATETIME
@@ -35,44 +13,73 @@ BEGIN
 
 			MERGE [ODS].[dbo].[CT_PatientPharmacy] AS a
 				USING(SELECT Distinct 
-					  P.[PatientCccNumber] AS PatientID, P.[PatientPID] AS PatientPK,F.[Name] AS FacilityName, F.Code AS SiteCode,PP.[VisitID] VisitID,PP.[Drug] Drug
-					  ,PP.[DispenseDate] DispenseDate,PP.[Duration] Duration,PP.[ExpectedReturn] ExpectedReturn,PP.[TreatmentType] TreatmentType
-					  ,PP.[PeriodTaken] PeriodTaken,PP.[ProphylaxisType] ProphylaxisType,P.[Emr] Emr
-					  ,CASE P.[Project] 
-								WHEN 'I-TECH' THEN 'Kenya HMIS II' 
-								WHEN 'HMIS' THEN 'Kenya HMIS II'
-							ELSE P.[Project] 
-					   END AS [Project] 
-					  ,PP.[Voided] Voided
-					  ,PP.[Processed] Processed
-					  ,PP.[Provider] [Provider]
-					  ,PP.[RegimenLine] RegimenLine
-					  ,PP.[Created] Created
-					  ,PP.RegimenChangedSwitched RegimenChangedSwitched
-					  ,PP.RegimenChangeSwitchReason RegimenChangeSwitchReason
-					  ,PP.StopRegimenReason StopRegimenReason
-					  ,PP.StopRegimenDate StopRegimenDate,					  
-					  PP.ID, PP.[Date_Created],PP.[Date_Last_Modified]
-					  ,PP.RecordUUID
+							P.[PatientCccNumber] AS PatientID
+							,P.[PatientPID] AS PatientPK
+							,F.[Name] AS FacilityName 
+							,F.Code AS SiteCode
+							,PP.[VisitID] As VisitID
+							,PP.[Drug] As Drug
+							,PP.[DispenseDate] AS DispenseDate
+							,PP.[Duration] As Duration
+							,PP.[ExpectedReturn] As ExpectedReturn
+							,PP.[TreatmentType] As TreatmentType
+							,PP.[PeriodTaken] As PeriodTaken
+							,PP.[ProphylaxisType] As ProphylaxisType
+							,P.[Emr] As Emr
+							,CASE P.[Project] 
+								WHEN 'I-TECH'	THEN 'Kenya HMIS II' 
+								WHEN 'HMIS'		THEN 'Kenya HMIS II'
+								ELSE P.[Project] 
+							END AS [Project] 
+							,PP.[Voided] As Voided
+							,PP.[Processed]  As Processed
+							,PP.[Provider] As  [Provider]
+							,PP.[RegimenLine] As RegimenLine
+							,PP.[Created]As Created
+							,PP.RegimenChangedSwitched As RegimenChangedSwitched
+							,PP.RegimenChangeSwitchReason As RegimenChangeSwitchReason
+							,PP.StopRegimenReason As StopRegimenReason
+							,PP.StopRegimenDate As StopRegimenDate				  
+							,PP.ID
+							,PP.[Date_Created]
+							,PP.[Date_Last_Modified]
+							,PP.RecordUUID
 						FROM [DWAPICentral].[dbo].[PatientExtract] P 
-						INNER JOIN [DWAPICentral].[dbo].[PatientPharmacyExtract] PP ON PP.[PatientId]= P.ID 
-						INNER JOIN [DWAPICentral].[dbo].[Facility] F ON P.[FacilityId] = F.Id AND F.Voided=0
-					WHERE p.gender!='Unknown' AND F.code >0) AS b 
-						ON(
-						 a.SiteCode = b.SiteCode
-						and  a.PatientPK  = b.PatientPK 
-						and a.visitID = b.visitID
-						and a.DispenseDate = b.DispenseDate
-						and a.voided   = b.voided
-						and a.ID =b.ID						
+							INNER JOIN [DWAPICentral].[dbo].[PatientPharmacyExtract] PP ON PP.[PatientId]= P.ID 
+							INNER JOIN [DWAPICentral].[dbo].[Facility] F ON P.[FacilityId] = F.Id AND F.Voided=0
+							INNER JOIN (
+										SELECT  F.code as SiteCode
+												,p.[PatientPID] as PatientPK
+												,InnerPP.voided
+												,InnerPP.DispenseDate
+												,max(InnerPP.ID) As maxID
+												,MAX(InnerPP.created )AS Maxdatecreated
+										FROM [DWAPICentral].[dbo].[PatientExtract] P WITH (NoLock)  						
+											INNER JOIN [DWAPICentral].[dbo].[PatientPharmacyExtract]  InnerPP WITH(NoLock)  ON InnerPP.[PatientId]= P.ID 
+											INNER JOIN [DWAPICentral].[dbo].[Facility] F WITH(NoLock)  ON P.[FacilityId] = F.Id AND F.Voided=0
+										GROUP BY F.code,p.[PatientPID],InnerPP.voided,InnerPP.DispenseDate
+							) tm 
+							ON	f.code = tm.[SiteCode] and 
+								p.PatientPID=tm.PatientPK and 
+								PP.voided = tm.voided and 
+								PP.created = tm.Maxdatecreated and
+								PP.ID =tm.maxID  and
+								PP.DispenseDate = tm.DispenseDate
 
+						WHERE p.gender!='Unknown' AND F.code >0
+					) AS b 
+						ON(
+							 a.SiteCode		= b.SiteCode and
+							 a.PatientPK	= b.PatientPK and
+							 a.DispenseDate = b.DispenseDate and
+							 a.voided		= b.voided					
 						)
 
-				WHEN NOT MATCHED THEN 
+			WHEN NOT MATCHED THEN 
 					INSERT(ID,PatientID,SiteCode,FacilityName,PatientPK,VisitID,Drug,DispenseDate,Duration,ExpectedReturn,TreatmentType,PeriodTaken,ProphylaxisType,Emr,Project,RegimenLine,RegimenChangedSwitched,RegimenChangeSwitchReason,StopRegimenReason,StopRegimenDate, [Date_Created],[Date_Last_Modified],RecordUUID,voided,LoadDate) 
 					VALUES(ID,PatientID,SiteCode,FacilityName,PatientPK,VisitID,Drug,DispenseDate,Duration,ExpectedReturn,TreatmentType,PeriodTaken,ProphylaxisType,Emr,Project,RegimenLine,RegimenChangedSwitched,RegimenChangeSwitchReason,StopRegimenReason,StopRegimenDate, [Date_Created],[Date_Last_Modified],RecordUUID,voided,Getdate())
 			
-				WHEN MATCHED THEN
+			WHEN MATCHED THEN
 					UPDATE SET 
 						a.PatientID					=b.PatientID,
 						a.FacilityName				=b.FacilityName,
@@ -82,11 +89,10 @@ BEGIN
 						a.RegimenChangedSwitched	=b.RegimenChangedSwitched,
 						a.RegimenChangeSwitchReason	=b.RegimenChangeSwitchReason,
 						a.StopRegimenReason			=b.StopRegimenReason,
-						 a.[Date_Created]			=b.[Date_Created],
-						 a.[Date_Last_Modified]		=b.[Date_Last_Modified],
-						  a.RecordUUID			=b.RecordUUID,
-						a.voided		=b.voided
-						 ;
+						a.[Date_Created]			=b.[Date_Created],
+						a.[Date_Last_Modified]		=b.[Date_Last_Modified],
+						a.RecordUUID				=b.RecordUUID,
+						a.voided					=b.voided;
 
 						
 				UPDATE [ODS_logs].[dbo].[CT_PharmacyVisit_Log]
