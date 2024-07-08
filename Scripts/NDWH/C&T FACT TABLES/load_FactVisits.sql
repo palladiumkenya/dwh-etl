b5cb3995c7133f17adaf186b0ae2150b85125559
Marymary-dev <marywambu@gmail.com>
2024-07-08 21:49:27 +0300

    Removing duplicate visits records

--- Scripts/NDWH/C&T FACT TABLES/load_FactVisits.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactVisits.sql
@@ -1,6 +1,6 @@
 
 IF OBJECT_ID(N'[NDWH].[dbo].[FactVisits]', N'U') IS NOT NULL 
-	DROP TABLE [NDWH].[dbo].[FactVisits];
+	DROP TABLE NDWH.[dbo].[FactVisits];
 
 
 BEGIN	
@@ -10,7 +10,19 @@ with MFL_partner_agency_combination as (
 		SDP,
 	    SDP_Agency as Agency 
 	from ODS.dbo.All_EMRSites 
+),
+UniqueVisits as (
+Select   row_number() OVER (PARTITION BY SiteCode,Patientpkhash, VisitDate ORDER BY VisitDate DESC) AS NUM,
+	Patientpkhash,
+	Sitecode,
+	VisitDate,
+	WHOStage,
+	NextAppointmentDate,
+	voided
+	from ODS.dbo.CT_PatientVisits as visits
+	
 )
+
 select 
 	Factkey = IDENTITY(INT, 1, 1),
 	patient.PatientKey,
@@ -23,9 +35,9 @@ select
 	WHOStage,
 	cast(getdate() as date) as LoadDate
 into NDWH.dbo.FactVisits
-from ODS.dbo.CT_PatientVisits as visits
-left join ODS.dbo.CT_ARTPatients as art on art.PatientPKHash=visits.PatientPKHash and art.SiteCode=visits.SiteCode
-left join NDWH.dbo.DimPatient as patient on visits.PatientPKHash = patient.PatientPKHash and visits.SiteCode = patient.SiteCode
+from UniqueVisits as  visits
+inner join ODS.dbo.CT_ARTPatients as art on art.PatientPKHash=visits.PatientPKHash and art.SiteCode=visits.SiteCode
+inner join NDWH.dbo.DimPatient as patient on visits.PatientPKHash = patient.PatientPKHash and visits.SiteCode = patient.SiteCode
 left join NDWH.dbo.DimFacility as facility on facility.MFLCode = visits.SiteCode
 left join MFL_partner_agency_combination on MFL_partner_agency_combination.MFL_Code = visits.SiteCode
 left join NDWH.dbo.DimPartner as partner on partner.PartnerName = MFL_partner_agency_combination.SDP
@@ -33,8 +45,12 @@ left join NDWH.dbo.DimAgency as agency on agency.AgencyName = MFL_partner_agency
 left join NDWH.dbo.DimDate as VisitDate on VisitDate.Date=visits.VisitDate
 left join NDWH.dbo.DimDate as StartARTDate on StartARTDate.Date=art.StartARTDate
 left join NDWH.dbo.DimDate as NextAppointmentDate on NextAppointmentDate.Date=visits.NextAppointmentDate
-WHERE Visits.voided =0 and  VisitDate >= EOMONTH(DATEADD(MONTH, -12, GETDATE()))
+WHERE Visits.voided =0 and Visits.NUM=1 and VisitDate >= EOMONTH(DATEADD(MONTH, -11, GETDATE())) 
 
 
-alter table NDWH.dbo.FactVisits add primary key(FactKey);
+alter table HIVCaseSurveillance.dbo.FactVisits add primary key(FactKey);
 END
+
+
+
+ 
