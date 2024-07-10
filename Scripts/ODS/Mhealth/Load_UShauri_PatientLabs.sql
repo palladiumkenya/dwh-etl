a18428ed3336a5ef98150915da09a5ae8256b53e
NewtonMutugi <newtonmut197@gmail.com>
2024-07-10 16:52:40 +0300

    Added Ushauri PatientLabs Scripts

---
+++ Scripts/ODS/Mhealth/Load_UShauri_PatientLabs.sql
@@ -0,0 +1,28 @@
+---- Loads data from MhealthCentral to ODS
+BEGIN
+
+  MERGE [ODS].[dbo].[Ushauri_PatientLabs] AS a
+  USING (
+    SELECT DISTINCT [PatientPK], [PatientPKHash], [PatientID], [PatientIDHash], [SiteCode], [FacilityName], [OrderedbyDate], [ReportedbyDate], [TestName], [TestResult], [Units], [LabName], [CT_PatientPK]
+    FROM [MhealthCentral].[dbo].[CT_PatientLabs] (NOLOCK)
+  ) AS b
+  ON (a.[PatientID] = b.[PatientID])
+  WHEN NOT MATCHED THEN
+    INSERT ([PatientPK], [Ushauri_PatientPK], [PatientPKHash], [PatientID], [PatientIDHash], [SiteCode], [FacilityName], [OrderedbyDate], [ReportedbyDate], [TestName], [TestResult], [Units], [LabName])
+    VALUES (b.[PatientPK], b.[PatientPK], b.[PatientPKHash], b.[PatientID], b.[PatientIDHash], b.[SiteCode], b.[FacilityName], b.[OrderedbyDate], b.[ReportedbyDate], b.[TestName], b.[TestResult], b.[Units], b.[LabName])
+    WHEN MATCHED THEN
+    UPDATE
+    SET
+      a.[PatientPKHash] = b.[PatientPKHash],
+      a.[PatientIDHash] = b.[PatientIDHash],
+      a.[SiteCode] = b.[SiteCode],
+      a.[FacilityName] = b.[FacilityName],
+      a.[OrderedbyDate] = b.[OrderedbyDate],
+      a.[ReportedbyDate] = b.[ReportedbyDate],
+      a.[TestName] = b.[TestName],
+      a.[TestResult] = b.[TestResult],
+      a.[Units] = b.[Units],
+      a.[LabName] = b.[LabName],
+      a.[Ushauri_PatientPK] = b.[PatientPK];
+
+END;
