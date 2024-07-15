7ed81ca5f81584ee12e2bffbdde368e8a4b17824
NewtonMutugi <newtonmut197@gmail.com>
2024-07-15 12:19:36 +0300

    Removed SiteCode from join on 9_Update_CT_PatientLabs.sql

--- Scripts/ODS/Mhealth/Post_Load/9_Update_CT_PatientLabs.sql
+++ Scripts/ODS/Mhealth/Post_Load/9_Update_CT_PatientLabs.sql
@@ -11,5 +11,5 @@ SET
     a.PatientPK = p.PatientPK
 FROM
     [ODS].[dbo].[Ushauri_PatientLabs] a
-	JOIN [ODS].[dbo].[CT_PatientLabs] p
-		ON a.SiteCode = p.SiteCode AND a.PatientID = p.PatientID;
+	JOIN [ODS].[dbo].[CT_Patient] p
+		ON a.PatientID = p.PatientID;
