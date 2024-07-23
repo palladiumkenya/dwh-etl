826d7ff5bb8478a5e47e32c4bf2e3d36b56ee5b7
NewtonMutugi <newtonmut197@gmail.com>
2024-07-23 15:05:44 +0300

    Update table names for Mhealth applications (post load scripts)

--- Scripts/ODS/Mhealth/Post_Load/1_RecreatePatient_PatientPKColumns.sql
+++ Scripts/ODS/Mhealth/Post_Load/1_RecreatePatient_PatientPKColumns.sql
@@ -1,46 +1,46 @@
 /* Renaming patientPK column coming from mhealth to UshauriPatientPK for the purpose of matching patient from C&T to  ones coming from ushauri. */
 BEGIN
-  
 
-		IF EXISTS (SELECT * FROM sys.columns      /* 1st if confirms if the PatientPK column exists on [ODS].[dbo].[Ushauri_Patient] exists on ODS   */
-					WHERE Name = N'PatientPK' 
-					AND Object_ID = Object_ID(N'[ODS].[dbo].[Ushauri_Patient]'))
+
+		IF EXISTS (SELECT * FROM sys.columns      /* 1st if confirms if the PatientPK column exists on [ODS].[dbo].[Mhealth_Ushauri_Patient] exists on ODS   */
+					WHERE Name = N'PatientPK'
+					AND Object_ID = Object_ID(N'[ODS].[dbo].[Mhealth_Ushauri_Patient]'))
 		BEGIN
 			  IF  NOT EXISTS (SELECT *					/* If above condition is met, check if Ushauri_Patient exists. If it exists escape. If it doesn't exist create it*/
 							 FROM   INFORMATION_SCHEMA.COLUMNS
 							 WHERE  TABLE_NAME = 'Ushauri_Patient'
-							 AND COLUMN_NAME = 'UshauriPatientPK') 
+							 AND COLUMN_NAME = 'UshauriPatientPK')
 
 					BEGIN
-						EXEC sp_rename '[ODS].[dbo].[Ushauri_Patient].PatientPK', 'UshauriPatientPK', 'COLUMN';
+						EXEC sp_rename '[ODS].[dbo].[Mhealth_Ushauri_Patient].PatientPK', 'UshauriPatientPK', 'COLUMN';
 					END
 
 		END
 
-		IF EXISTS (SELECT * FROM sys.columns      /* 1st if confirms if the PatientPK column exists on [ODS].[dbo].[Ushauri_Patient] exists on ODS   */
-				WHERE Name = N'PatientPKHash' 
-				AND Object_ID = Object_ID(N'[ODS].[dbo].[Ushauri_Patient]'))
+		IF EXISTS (SELECT * FROM sys.columns      /* 1st if confirms if the PatientPK column exists on [ODS].[dbo].[Mhealth_Ushauri_Patient] exists on ODS   */
+				WHERE Name = N'PatientPKHash'
+				AND Object_ID = Object_ID(N'[ODS].[dbo].[Mhealth_Ushauri_Patient]'))
 		BEGIN
 			  IF  NOT EXISTS (SELECT *					/* If above condition is met, check if Ushauri_Patient exists. If it exists escape. If it doesn't exist create it*/
 							FROM   INFORMATION_SCHEMA.COLUMNS
-							WHERE  TABLE_NAME = 'Ushauri_Patient'
-							AND COLUMN_NAME = 'PatientPKHash') 
+							WHERE  TABLE_NAME = 'Mhealth_Ushauri_Patient'
+							AND COLUMN_NAME = 'PatientPKHash')
 
 					BEGIN
-						EXEC sp_rename '[ODS].[dbo].[Ushauri_Patient].PatientPKHash', 'PatientPKHash', 'COLUMN';
+						EXEC sp_rename '[ODS].[dbo].[Mhealth_Ushauri_Patient].PatientPKHash', 'PatientPKHash', 'COLUMN';
 					END
 
 		END
-		
+
 		IF NOT EXISTS (
 			  SELECT
 				*
 			  FROM
 				INFORMATION_SCHEMA.COLUMNS
 			  WHERE
-				TABLE_NAME = 'Ushauri_Patient' AND COLUMN_NAME = 'patientPK')
+				TABLE_NAME = 'Mhealth_Ushauri_Patient' AND COLUMN_NAME = 'patientPK')
 			BEGIN
-			  ALTER TABLE [ODS].[dbo].[Ushauri_Patient]
+			  ALTER TABLE [ODS].[dbo].[Mhealth_Ushauri_Patient]
 				ADD patientPK int NULL
 			END;
 
@@ -50,9 +50,9 @@ BEGIN
 			  FROM
 				INFORMATION_SCHEMA.COLUMNS
 			  WHERE
-				TABLE_NAME = 'Ushauri_Patient' AND COLUMN_NAME = 'PatientPKHash')
+				TABLE_NAME = 'Mhealth_Ushauri_Patient' AND COLUMN_NAME = 'PatientPKHash')
 			BEGIN
-			  alter table [ODS].[dbo].[Ushauri_Patient]
+			  alter table [ODS].[dbo].[Mhealth_Ushauri_Patient]
 					add PatientPKHash nvarchar(150) null
 			END;
 
@@ -62,9 +62,9 @@ BEGIN
 			  FROM
 				INFORMATION_SCHEMA.COLUMNS
 			  WHERE
-				TABLE_NAME = 'Ushauri_Patient' AND COLUMN_NAME = 'UshauriPatientPKHash')
+				TABLE_NAME = 'Mhealth_Ushauri_Patient' AND COLUMN_NAME = 'UshauriPatientPKHash')
 			BEGIN
-			  alter table [ODS].[dbo].[Ushauri_Patient]
+			  alter table [ODS].[dbo].[Mhealth_Ushauri_Patient]
 					add UshauriPatientPKHash nvarchar(150) null
 			END;
 
