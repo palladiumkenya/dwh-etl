1a5d8de389d16415b16a5b19d340a20d16ec8c77
Mumo Ngungu <mumongungu@gmail.com>
2024-07-30 01:19:11 +0300

    adding a new reporting table for risk factors for CS

---
+++ Scripts/REPORTING/load_cs_aggregate_alcoho_sex_sti_risk_factors.sql
@@ -0,0 +1,79 @@
+IF OBJECT_ID(N'HIVCaseSurveillance.dbo.CsAggregateAlcoholSex&STIRiskFactors', N'U') IS NOT NULL 
+	DROP TABLE HIVCaseSurveillance.dbo.CsAggregateAlcoholSex&STIRiskFactors;
+
+with cases as (
+  select 
+    art.PatientKey,
+    art.FacilityKey,
+    art.AgeGroupKey,
+    art.PartnerKey,
+    art.AgencyKey,
+    pat.Gender,
+    confirm_date.[Date] as DateConfirmedHIVPos
+  from NDWH.dbo.FactART as art 
+  left join NDWH.dbo.DimPatient as pat on pat.PatientKey = art.PatientKey
+  left join NDWH.dbo.DimDate as confirm_date on confirm_date.DateKey = pat.DateConfirmedHIVPositiveKey
+  where confirm_date.Date > DATEADD(MONTH, -12, EOMONTH(GETDATE()))
+),
+eligibility_indicators as (
+  select
+    row_number() over (partition by eligibility_data.PatientKey order by eligibilitydate.date desc) as rank,
+    eligibility_data.PatientKey,
+    eligibilitydate.Date as ELigibilityVisitDate,
+    case 
+      when AlcoholSex in ('Always', 'Sometimes') then 1
+      else 0
+    end as SexwithAlcohoDrugs,
+    case 
+      when EverHadSTI = 'Yes' then 1 
+      else 0 
+    end as EverHadSTI
+from NDWH.dbo.FactHTSEligibilityextract as eligibility_data
+left join NDWH.dbo.DimDate as eligibilitydate on eligibilitydate.DateKey = eligibility_data.VisitDateKey
+),
+latest_eligibility_indicators_per_patient as (
+  select 
+    *
+  from eligibility_indicators
+  where rank = 1
+),
+joined_data as (
+  select 
+    cases.PatientKey,
+    cases.FacilityKey,
+    cases.AgeGroupKey,
+    cases.PartnerKey,
+    cases.AgencyKey,
+    cases.Gender,
+    cases.DateConfirmedHIVPos,
+    eligibility.ELigibilityVisitDate,
+    SexwithAlcohoDrugs,
+    EverHadSTI
+  from cases
+  inner join latest_eligibility_indicators_per_patient as eligibility on eligibility.PatientKey = cases.PatientKey
+)
+select 
+  eomonth(confirm_date.Date) as CohortYearMonth,
+  facility.FacilityName,
+  facility.County,
+  facility.SubCounty,
+  partner.PartnerName,
+  agency.AgencyName,
+  count(joined_data.PatientKey) as NoOfCases,
+  sum(SexwithAlcohoDrugs) HasSexwithAlcohoDrugs,
+  sum(EverHadSTI) as EverHadSTI
+into HIVCaseSurveillance.dbo.CsAggregateAlcoholSex&STIRiskFactors
+from joined_data
+left join NDWH.dbo.DimPartner as partner on partner.PartnerKey = joined_data.PartnerKey
+left join NDWH.dbo.DimAgency as agency on agency.AgencyKey = joined_data.AgencyKey
+left join NDWH.dbo.DimAgeGroup as agegroup on agegroup.AgeGroupKey = joined_data.AgeGroupKey
+left join NDWH.dbo.DimPatient as patient on patient.PatientKey = joined_data.PatientKey
+left join NDWH.dbo.DimFacility as facility on facility.FacilityKey = joined_data.FacilityKey
+left join NDWH.dbo.DimDate as confirm_date on confirm_date.DateKey = patient.DateConfirmedHIVPositiveKey
+group by 
+    eomonth(confirm_date.Date),
+    facility.FacilityName,
+    facility.County,
+    facility.SubCounty,
+    partner.PartnerName,
+    agency.AgencyName; 
