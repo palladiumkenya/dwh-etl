78c01afaa3d110a51b59f529b5cb910a1a70ecec
NewtonMutugi <newtonmut197@gmail.com>
2024-08-03 23:52:40 +0300

    Modified to only get count CT_Relationships.sql

--- Scripts/ODS/DQC/Get_Voided_Null_Patients/CT_Relationships.sql
+++ Scripts/ODS/DQC/Get_Voided_Null_Patients/CT_Relationships.sql
@@ -1,5 +1,8 @@
-SELECT [SiteCode],Count(1)NullPatientPK_IDHash
+WITH cte AS (
+	SELECT [SiteCode],Count(1)NullPatientPK_IDHash
 	FROM [ODS].[dbo].[CT_Relationships]
 	WHERE PatientPKHash IS NULL OR PatientIDHash IS NULL AND [Voided] = 0
 	GROUP BY [SiteCode]
 	HAVING Count(1) > 1
+)
+SELECT COUNT(*) FROM cte
