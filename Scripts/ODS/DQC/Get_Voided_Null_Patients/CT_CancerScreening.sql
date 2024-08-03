efd7ebef382862b6c422440ef3a597bdb8d07f9d
NewtonMutugi <newtonmut197@gmail.com>
2024-08-03 22:14:07 +0300

    Modified to only get count CT_CancerScreening.sql

--- Scripts/ODS/DQC/Get_Voided_Null_Patients/CT_CancerScreening.sql
+++ Scripts/ODS/DQC/Get_Voided_Null_Patients/CT_CancerScreening.sql
@@ -1,5 +1,8 @@
-SELECT [Sitecode],Count(1)NullPatientPK_IDHash
+WITH cte AS(
+	SELECT [Sitecode],Count(1)NullPatientPK_IDHash
 	FROM [ODS].[dbo].[CT_CancerScreening]
 	WHERE PatientPKHash IS NULL OR PatientIDHash IS NULL AND [Voided] = 0
 	GROUP BY [SiteCode]
 	HAVING Count(1) > 1
+)
+SELECT COUNT(*) FROM cte
