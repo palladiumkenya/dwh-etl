9e24ad5a8bcd8f085e8abd50b54ad4db7d72b3f4
Mumo Ngungu <mumongungu@gmail.com>
2024-08-13 10:22:54 +0300

    adding reporting table for index elicitation cascade

--- Scripts/NDWH/C&T FACT TABLES/load_FactContactElicitation.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactContactElicitation.sql
@@ -21,18 +21,27 @@ subset_data as (
         ContactAge,
         ContactMaritalStatus,
         RelationshipWithPatient,
+		KnowledgeOfHivStatus,
         DateCreated
     from ODS.dbo.CT_ContactListing
     where voided = 0 
 ),
-tested_contacts as(
+tested_contacts as (
     select 
-        distinct ContactPatientPK,
+        ContactPatientPK,
         subset_data.SiteCode,
-        TestDate
+		FinalTestResult,
+        TestDate,
+        row_number() over (partition by subset_data.ContactPatientPK, subset_data.SiteCode order by TestDate desc) as rank
     from subset_data
     inner join ODS.dbo.HTS_ClientTests as tests on tests.PatientPk = subset_data.ContactPatientPK
         and tests.SiteCode =subset_data.SiteCode
+),
+latest_test_for_tested_contacts as (
+    select 
+        *
+    from tested_contacts
+    where rank = 1
 )
 select 
 	Factkey = IDENTITY(INT, 1, 1),
@@ -46,12 +55,17 @@ select
     ContactAge,
     ContactMaritalStatus,
     RelationshipWithPatient,
+	case 
+		when KnowledgeOfHivStatus = 'Positive' then 'Positive'
+		when KnowledgeOfHivStatus = 'Negative' then 'Negative'
+	end as ContactHIVStatusAtElicitation,
     created.DateKey as DateCreatedKey,
-    case when tested_contacts.ContactPatientPK is not null then 1 else 0 end as Tested
+    case when latest_test_for_tested_contacts.ContactPatientPK is not null then 1 else 0 end as Tested,
+	latest_test_for_tested_contacts.FinalTestResult as ContactHIVStatusAfterTesting
 into [NDWH].[dbo].[FactContactElicitation]
 from subset_data
-left join tested_contacts on tested_contacts.ContactPatientPK = subset_data.ContactPatientPK
-    and tested_contacts.SiteCode = subset_data.SiteCode
+left join latest_test_for_tested_contacts on latest_test_for_tested_contacts.ContactPatientPK = subset_data.ContactPatientPK
+    and latest_test_for_tested_contacts.SiteCode = subset_data.SiteCode
 left join NDWH.dbo.DimPatient as index_pat on index_pat.PatientPKHash = subset_data.PatientPKHash
     and index_pat.SiteCode = subset_data.SiteCode
 left join NDWH.dbo.DimPatient as contact_pat on contact_pat.PatientPKHash = subset_data.ContactPatientPKHash
