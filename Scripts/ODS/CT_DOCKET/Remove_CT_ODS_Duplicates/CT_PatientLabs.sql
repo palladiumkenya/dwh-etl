701f2fa2b898b31644f4408deeeaf4bfa1b64deb
Mugo <dennis.ndwiga@thepalladiumgroup.com>
2024-08-15 14:42:04 +0300

    Added ) after nolock

--- Scripts/ODS/CT_DOCKET/Remove_CT_ODS_Duplicates/CT_PatientLabs.sql
+++ Scripts/ODS/CT_DOCKET/Remove_CT_ODS_Duplicates/CT_PatientLabs.sql
@@ -6,7 +6,7 @@
 						TestName,
 						 ROW_NUMBER() OVER (PARTITION BY PatientPK,Sitecode,OrderedbyDate,TestResult,TestName ORDER BY
 						OrderedbyDate) Row_Num
-						FROM [ODS].[dbo].[CT_PatientLabs](NoLock)
+						FROM [ODS].[dbo].[CT_PatientLabs](NoLock))
 					DELETE from cte
 						Where Row_Num >1 ;
 
