3c7c51e9fa2a8a60075574ad6eee884783f49b66
Marymary-dev <marywambu@gmail.com>
2024-09-18 15:32:05 +0300

    Adding other CD4s

--- Scripts/NDWH/C&T FACT TABLES/load_FactCD4.sql
+++ Scripts/NDWH/C&T FACT TABLES/load_FactCD4.sql
@@ -25,6 +25,25 @@ with MFL_partner_agency_combination as (
 	  where RowNum=1
 	  ),
 
+      BCD4s as (SELECT 
+        ROW_NUMBER() OVER (PARTITION BY PatientPK, Sitecode ORDER BY OrderedbyDate Asc) AS RowNum,
+        PatientPKHash,
+        PatientPk,
+        SiteCode,
+		OrderedbyDate,
+        TestName,
+        TestResult
+    FROM ODS.dbo.CT_PatientLabs
+    WHERE 
+        TestName like '%CD4%'
+      ),
+
+	  BaselineCD4s as (Select * from BCD4s
+	  where RowNum=1
+	  ),
+      OtherCD4s as (Select * from CD4s
+      Where RowNum=2
+      ),
 source_CD4 as (
 	select
 		distinct baselines.PatientIDHash,
@@ -32,18 +51,22 @@ source_CD4 as (
 		baselines.SiteCode,
 		CD4atEnrollment,
 		CD4atEnrollment_Date as CD4atEnrollmentDate,
-		bCD4 as BaselineCD4,
-		bCD4Date as BaselineCD4Date,
+		BaselineCD4s.TestResult as BaselineCD4,
+		BaselineCD4s.OrderedbyDate as BaselineCD4Date,
 		LatestCD4s.OrderedbyDate as LastCD4Date,
         Case When LatestCD4s.TestName='CD4 Count'Then LatestCD4s.TestResult Else Null End as LastCD4,
         Case When LatestCD4s.TestName='CD4 Percentage' Then LatestCD4s.TestResult Else Null End as LastCD4Percentage,
-		datediff(yy, patient.DOB, last_encounter.LastEncounterDate) as AgeLastVisit
+		datediff(yy, patient.DOB, last_encounter.LastEncounterDate) as AgeLastVisit,
+        OtherCD4s.TestResult as OtherCD4s,
+        OtherCD4s.OrderedbyDate as OtherCD4sDate
 	from ODS.dbo.CT_PatientBaselines as baselines
 	left join ODS.dbo.CT_Patient as patient on patient.PatientPK = baselines.PatientPK
 	and patient.SiteCode = baselines.SiteCode
 	left join ODS.dbo.Intermediate_LastPatientEncounter as last_encounter on last_encounter.PatientPK = baselines.PatientPK
 		and last_encounter.SiteCode = baselines.SiteCode
-        left join LatestCD4s on LatestCD4s.PatientPK=baselines.PatientPK and LatestCD4s.Sitecode=baselines.SiteCode
+    left join LatestCD4s on LatestCD4s.PatientPK=baselines.PatientPK and LatestCD4s.Sitecode=baselines.SiteCode
+    left join BaselineCD4s on BaselineCD4s.PatientPk=baselines.PatientPK and BaselineCD4s.SiteCode=baselines.SiteCode
+    left join OtherCD4s on OtherCD4s.Patientpk=baselines.Patientpk and OtherCD4s.Sitecode=baselines.Sitecode
 )
 
 select 
@@ -76,3 +99,4 @@ WHERE patient.voided =0;
 
 alter table NDWH.dbo.FactCD4 add primary key(FactKey);
 END
+
