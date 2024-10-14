c4dc6e932b5792c8bfe8aba054665e7b5cbf4004
Mumo Ngungu <mumongungu@gmail.com>
2024-10-14 15:07:41 +0300

    test

--- Scripts/REPORTING/load_aggregate_dsd.sql
+++ Scripts/REPORTING/load_aggregate_dsd.sql
@@ -1,42 +1,42 @@
-IF OBJECT_ID(N'[REPORTING].[dbo].[AggregateDSD]', N'U') IS NOT NULL 
-	drop TABLE [REPORTING].[dbo].[AggregateDSD]
-GO
+if OBJECT_ID(N'[REPORTING].[dbo].[AggregateDSD]', N'U') is not null
+  drop table Reporting.Dbo.Aggregatedsd
+go
 
-SELECT DISTINCT
-    MFLCode,
-    f.FacilityName,
-    County,
-    SubCounty,
-    p.PartnerName,
-    a.AgencyName,
-    Gender,
-    age.DATIMAgeGroup as AgeGroup, 
-    StabilityAssessment,
-	DifferentiatedCare,
-    SUM(onMMD) as patients_onMMD,
-    SUM(case when onMMD = 0 then 1 else 0 end) as patients_nonMMD,
-    COUNT(StabilityAssessment) AS Stability,
-    Sum(pat.isTXCurr) As TXCurr,
-    cast(getdate() as date) as LoadDate
-INTO [REPORTING].[dbo].[AggregateDSD]
-FROM NDWH.dbo.FactART as art
-LEFT JOIN NDWH.dbo.FactLatestObs as lob on lob.Patientkey = art.PatientKey
-LEFT JOIN NDWH.dbo.DimAgeGroup age on age.AgeGroupKey = art.AgeGroupKey
-LEFT JOIN NDWH.dbo.DimFacility f on f.FacilityKey = art.FacilityKey
-LEFT JOIN NDWH.dbo.DimAgency a on a.AgencyKey = art.AgencyKey
-LEFT JOIN NDWH.dbo.DimPatient pat on pat.PatientKey = art.PatientKey
-LEFT JOIN NDWH.dbo.DimPartner p on p.PartnerKey = art.PartnerKey
-WHERE pat.IsTXCurr = 1
-GROUP BY 
-    MFLCode, 
-    f.FacilityName, 
-    County, 
-    SubCounty, 
-    p.PartnerName,
-    a.AgencyName, 
-    Gender, 
-    age.DATIMAgeGroup, 
-    StabilityAssessment,
-    DifferentiatedCare
+select distinct
+  Mflcode,
+  F.Facilityname,
+  County,
+  Subcounty,
+  P.Partnername,
+  A.Agencyname,
+  Gender,
+  Age.Datimagegroup as Agegroup,
+  Stabilityassessment,
+  Differentiatedcare,
+  cast(GETDATE() as date) as Loaddate,
+  SUM(Onmmd) as Patients_Onmmd,
+  SUM(case when Onmmd = 0 then 1 else 0 end) as Patients_Nonmmd,
+  COUNT(Stabilityassessment) as Stability,
+  SUM(Pat.Istxcurr) as Txcurr
+into Reporting.Dbo.Aggregatedsd
+from Ndwh.Dbo.Factart as Art
+left join Ndwh.Dbo.Factlatestobs as Lob on Art.Patientkey = Lob.Patientkey
+left join Ndwh.Dbo.Dimagegroup as Age on Art.Agegroupkey = Age.Agegroupkey
+left join Ndwh.Dbo.Dimfacility as F on Art.Facilitykey = F.Facilitykey
+left join Ndwh.Dbo.Dimagency as A on Art.Agencykey = A.Agencykey
+left join Ndwh.Dbo.Dimpatient as Pat on Art.Patientkey = Pat.Patientkey
+left join Ndwh.Dbo.Dimpartner as P on Art.Partnerkey = P.Partnerkey
+where Pat.Istxcurr = 1
+group by
+  Mflcode,
+  F.Facilityname,
+  County,
+  Subcounty,
+  P.Partnername,
+  A.Agencyname,
+  Gender,
+  Age.Datimagegroup,
+  Stabilityassessment,
+  Differentiatedcare
 
-GO
+go
